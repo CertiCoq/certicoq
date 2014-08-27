620166b63c8d660707c04d17870d43c882efa236
rpollack@seas.harvard.edu <rpollack@seas.harvard.edu@e2b0c78b-04ed-0310-b50c-832c51d4eb4a>
2014-08-27 16:02:13 +0000

    up-to-date version of Gregory Malecha's reflection plugin

---
+++ certicoq/reflection/template-coq/theories/Ast.v
@@ -0,0 +1,56 @@
+Require Import Coq.Strings.String.
+Require Import Coq.PArith.BinPos.
+
+Definition universe := positive.
+Definition ident := string.
+
+Inductive sort : Type :=
+| sProp
+| sSet
+| sType (_ : universe).
+
+Record ind : Type := {} .
+
+Inductive name : Type :=
+| nAnon
+| nNamed (_ : ident).
+
+Inductive cast_kind : Type :=
+| VmCast
+| NativeCast
+| Cast
+| RevertCast.
+
+Inductive inductive : Type :=
+| mkInd : string -> nat -> inductive.
+
+Record def (term : Type) : Type := mkdef
+{ dname : name (** the name (note, this may mention other definitions **)
+; dtype : term
+; dbody : term (** the body (a lambda term) **)
+; rarg  : nat  (** the index of the recursive argument **)
+}.
+
+Definition mfixpoint (term : Type) : Type :=
+  list (def term).
+
+Inductive term : Type :=
+| tRel       : nat -> term
+| tVar       : ident -> term
+| tMeta      : nat -> term
+| tEvar      : nat -> term
+| tSort      : sort -> term
+| tCast      : term -> cast_kind -> term -> term
+| tProd      : name -> term (** the type **) -> term -> term
+| tLambda    : name -> term (** the type **) -> term -> term
+| tLetIn     : name -> term (** the type **) -> term -> term -> term
+| tApp       : term -> list term -> term
+| tConst     : string -> term
+| tInd       : inductive -> term
+| tConstruct : inductive -> nat -> term
+| tCase      : term (** type info **) -> term -> list term -> term
+| tFix       : mfixpoint term -> nat -> term
+(*
+| CoFix     of ('constr, 'types) pcofixpoint
+*)
+| tUnknown : string -> term.

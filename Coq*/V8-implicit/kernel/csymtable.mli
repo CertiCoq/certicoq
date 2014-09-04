1c3401470cfcc157a496cb618544a2d6109b1f83
rpollack <rpollack0@users.noreply.github.com>
2014-09-04 18:35:03 +0000

    removed Coq* directory as it is very large and I can't compile Coq* in place because of the need to use old version of camlp5.

--- Coq*/V8-implicit/kernel/csymtable.mli
+++
@@ -1,8 +0,0 @@
-open Names
-open Term
-open Pre_env
-
-val val_of_constr : env -> constr -> values 
-
-val set_opaque_const      : constant -> unit
-val set_transparent_const : constant -> unit
--- Coq*/V8-implicit/theories/Reals/Rpow_def.v
+++
@@ -1,7 +0,0 @@
-Require Import Rdefinitions.
-
-Fixpoint pow (r:R) (n:nat) {struct n} : R :=
-  match n with
-    | O => R1
-    | S n => Rmult r (pow r n)
-  end.

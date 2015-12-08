f6fdd61a2ff3ec48e94f63e553a3bbced1abf9cd
olivierb <olivierb@e2b0c78b-04ed-0310-b50c-832c51d4eb4a>
2015-12-08 18:46:11 +0000

    Tests succeds for compilation from L1 to L6 and shrink-reduction of 0, idn and predn. Replaced ctx_bind_proj in L5_to_L6 by a version over nat (instead of N) as its termination proof was relying on (many) opaque theorems

--- certicoq/theories/L6_PCPS/tests.v
+++ certicoq/theories/L6_PCPS/tests.v
@@ -9,6 +9,7 @@ Require Import CpdtTactics.
 
 
 
+
 Add LoadPath "../common" as Common.
 Add LoadPath "../L1_MalechaQuoted" as L1.
 Add LoadPath "../L2_typeStrippedL1" as L2.
@@ -39,11 +40,29 @@ Definition shrink_once_L6 (oe: option exp): option exp :=
     | None => None
   end.
 
-Quote Recursively Definition p0l1 := 0%nat.
-Print p0l1.
-Definition p0l5 := Eval compute in compile p0l1.
-Print p0l5.
-Definition p0l6 := Eval compute in compile_to_L6 p0l5.
-Print p0l6.
-Definition p0l6s := Eval compute in shrink_once_L6 p0l6.
-Print p0l6s.
+
+
+
+Quote Recursively Definition p0L1 := 0%nat.
+Definition p0L5 := Eval compute in compile p0L1.
+Definition p0L6 := compile_to_L6 p0L5.
+Definition p0L6s := Eval compute in shrink_once_L6 p0L6.
+
+
+
+Fixpoint idn (n:nat) : nat := n.
+Quote Recursively Definition pIdnL1 := idn.
+Definition pIdnL5 := Eval compute in compile pIdnL1.
+Definition pIdnL6 := Eval compute in compile_to_L6 pIdnL5.
+Definition pIdnL6s := Eval compute in shrink_once_L6 pIdnL6.
+Print pIdnL6s.
+
+
+
+Definition predn (n : nat) : nat :=
+  match n with 0%nat => 0 | S n => n end.
+Quote Recursively Definition pPredL1 := predn.
+Definition pPredL5 := Eval compute in compile pPredL1.
+Definition pPredL6 := Eval compute in compile_to_L6 pPredL5.
+Definition pPredL6s := Eval compute in shrink_once_L6 (pPredL6).
+Print pPredL6s.

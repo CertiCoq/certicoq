2ded275d39120b3b871a69d3fbc165a7f02d4a04
abhishek.anand.iitg@gmail.com <abhishek.anand.iitg@gmail.com@e2b0c78b-04ed-0310-b50c-832c51d4eb4a>
2016-05-17 20:50:56 +0000

    now using a concrete instantiation of variables in L5a
    This is to avoid the limitations of Modules.
    (Why do we use Modules anyways, instead of first-class alternatives like Records and Typeclasses?)
    
    The concrete instance is defined in L4_deBruijn/VarInstance.v
    There are some admitted proofs in this file, but the computational parts are well defined, and ready to run.

---
+++ certicoq/theories/L4_deBruijn/VarInstance.v
@@ -0,0 +1,61 @@
+Require Import SquiggleLazyEq.export.
+Require Import SquiggleLazyEq.UsefulTypes.
+
+Require Import Coq.Arith.Arith Coq.NArith.BinNat Coq.Strings.String Coq.Lists.List Coq.omega.Omega 
+  Coq.Program.Program Coq.micromega.Psatz.
+
+Global Instance deqpos : Deq positive.
+Proof.
+  apply @deqAsSumbool.
+  intros ? ?. unfold DecidableSumbool.
+  decide equality.
+Qed.
+
+Definition varClassP (p:positive) : bool :=
+match p with
+| xO _ => true (* p is even *)
+| _  => false
+end.
+
+Fixpoint maxl (d: positive) (p: list positive) : positive :=
+match p with
+| [] => d
+| h::tl  => maxl (Pos.max h d) tl
+end.
+
+Fixpoint seq (start : positive) (len : nat) {struct len} : list positive :=
+match len with
+| 0 => []
+| S len0 => start :: seq (Pos.succ start) len0
+end.
+
+
+Definition freshVarsPosAux (n:nat) 
+  (c : bool) (avoid original : list positive) : list positive :=
+let maxn := maxl xH avoid in
+let f := (if c then xO else xI) in
+List.map f (seq (Pos.succ maxn) n).
+
+Definition freshVarsPos (n:nat) 
+  (c : option bool) (avoid original : list positive) : list positive :=
+let c : bool :=
+  match c with
+  |Some c => c
+  |None => true
+  end in
+  freshVarsPosAux n c avoid original.
+
+Lemma freshVarsPosCorrect:
+forall (n : nat) (oc : option bool) (avoid original : list positive),
+let lf := freshVarsPos n oc avoid original in
+(NoDup lf /\ list.disjoint lf avoid /\ Datatypes.length lf = n) /\
+(forall c : bool, oc = Some c -> forall v : positive, In v lf -> varClassP v = c).
+Proof.
+Admitted.
+
+Global Instance vartypePos : @VarType positive bool deqpos.
+  apply Build_VarType with (varClass:=varClassP) (freshVars := freshVarsPos).
+  exact freshVarsPosCorrect.
+Defined.
+
+(* Move all of this file to SquiggleLazyEq *)

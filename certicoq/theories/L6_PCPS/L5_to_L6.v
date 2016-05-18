6788a2b11800685348a47869f8e81df516dca0cd
olivierb <olivierb@e2b0c78b-04ed-0310-b50c-832c51d4eb4a>
2016-05-18 18:59:57 +0000

    Ported the translation in L5_to_L6 to the new language L5a (L5 instanciated with positives) and the updated L6. Will update tests.v later this week with examples going from L1 to L6

--- certicoq/theories/L6_PCPS/L5_to_L6.v
+++ certicoq/theories/L6_PCPS/L5_to_L6.v
@@ -1,23 +1,18 @@
 (* Convertion from 
-       cpstrans.cps  (L5)
+       L4.L5A.cps  (L5a)
               to
        cps.exp (L6)
     resulting in globally unique names  
 
-  2015 - Olivier Savary Belanger 
+  2016 - Olivier Savary Belanger 
 
-
-
-
-   DONE: fix recursive definition of list-based convert for termination checker
-      for some reason wouldn't accept termination even when rewritten with fold_right, had to pull up all 3 list-based convert as definitions 
-   DONE : modify convert_v s.t. it doesn't bind Var and KVar to a new name (e.g. by returning list of bindings) 
-   DONE: fix conversion of functions, branches to new syntax (datatypes instead of lists) 
    TODO : populate the types (ty atm) within expressions
 
-new
 
-*)     
+
+ *)
+
+
 Require Import Coq.ZArith.ZArith.
 Require Import Coq.ZArith.Znumtheory.
 Require Import ExtLib.Data.List.
@@ -29,33 +24,18 @@ Require Import CpdtTactics.
 
 
 Require Import ExtLib.Data.List.
+Require Import L4.VarInstance.
+Require Import L4.simpleCPSAA.
+Require Import L4.L5a.
 
-
-
-
-(* Add LoadPath "../L5_CPS" as CPS. *)
-(* Add LoadPath "../common" as Common. *)
-(* Add LoadPath "../L4_deBruijn" as L4. *)
-
-
-Require Import CPS.cpstrans. (* using cps for db cps terms *)
-Require Import cps. (* shadows exp from simple_cps for nominal cps terms *)
+Require Import cps.
 Require Import cps_util.
-
-
-(* returns list of numbers from n to n+m (excluding n+m) and the positive n+m (next available pos) *)
-Fixpoint fromN (n:positive) (m:nat) : list positive * positive :=
-  match m with
-  | O => (nil, n)
-  | S m' =>
-    let (l, nm ) := (fromN (n+1) (m')) in
-    (n::l, nm)
-  end.
+Require Import ctx.
 
 
 
-Section L5toL6.
 
+  
 (* placeholder *)
 Variable default_tag : M.elt.
 Variable var_tag : M.elt.
@@ -66,52 +46,46 @@ Variable ty_fun : positive. (* Regular function (lam) in simple_cps *)
 Variable ty_con : positive. (* continuation in simple_cps *)
 Variable ty : positive.  (* everything else *)
 
-Definition dcon_to_tag: dcon -> tag := N.succ_pos. (* should probably change dcon to positive in simple_cps *)
+Definition dcon_to_tag:dcon -> tag := N.succ_pos. (* should probably change dcon to positive in simple_cps *)
 
 
-(*  
-  NOTE:  use slower ctx_bind_proj' since this relies on opaque proofs
- 
-create a context binding m projections of var n to var (n+1+m)
- i.e. each binding has form  ({| let var (n + m + 1) := \pi_(m-1) var n in [] |})
-   
-  *)
-           
-Function ctx_bind_proj (n:positive) (m:N) {wf N.lt m}: exp_ctx :=
-  if N.eqb m 0%N then
-    Hole_c
-  else
-    let ctx_p' := ctx_bind_proj n (N.pred m) in
-    Eproj_c (Pos.add n (N.succ_pos m)) ty (N.pred m ) n ctx_p'.
-Proof.
-  intros. apply N.lt_pred_l.  apply N.eqb_neq in teq. assumption. apply N.lt_wf_0. 
-Defined.
-
-
-
-Fixpoint ctx_bind_proj' (n:positive) (m:nat): exp_ctx :=
+(* returns list of numbers [n, n+m[  and the positive n+m (next available pos) *)
+Fixpoint fromN (n:positive) (m:nat) : list positive * positive :=
   match m with
-    | O => Hole_c
+  | O => (nil, n)
+  | S m' =>
+    let (l, nm ) := (fromN (n+1) (m')) in
+    (n::l, nm)
+  end.
+
+
+(* Bind the m first projections of var r to variables [n, n+m[ *)
+Fixpoint ctx_bind_proj (r:positive) (m:nat) (n:var) : (exp_ctx * var) :=
+  match m with
+    | O => (Hole_c, n)
     | S m' =>
-      let ctx_p' := ctx_bind_proj' n m' in
-      Eproj_c (Pos.add n (Pos.of_nat m)) ty (N.of_nat m') n ctx_p'
+      let (ctx_p', n') := ctx_bind_proj r m' n in
+      (Eproj_c  n' ty (N.of_nat m') r ctx_p', Pos.succ n')
   end.
 
+Definition s_map := M.t var.
+Definition s_empty := M.empty var.
 
-
-
-Fixpoint fnlst_length (vs:fnlst) : nat :=
-  match vs with
-    | flnil => 0
-    | flcons e fds' => S (fnlst_length fds')
+Definition get_s (a:M.elt) (sig:s_map):=
+  match M.get a sig with
+    | None => (1%positive)
+    | Some v => v
   end.
 
-
-
-
+Fixpoint set_s_list (lx ly:list var) (sig:s_map) :=
+  match (lx, ly) with
+    | (x::lx', y::ly') =>
+      set_s_list lx' ly' (M.set x y sig)
+    | _ => sig
+  end.
 
 (* 
-  sv is a stack used to convert variable indices to name
+  sv is a map used to convert variable indices to name
 sk is a stack mapping continuation indices to name
 n is the next available name.
 
@@ -125,112 +99,107 @@ note : var "2" is taken as an external function standing for "halt"
       Continuation for pattern matching take a single argument whose m projections are bound to unique variables 
 
 
-*)
-Fixpoint convert (e:cps) (sv:list var) (sk:list var) (n:var) (*{| struct e |}*) : exp * var :=
+ *)
+
+Fixpoint convert (e:cps) (sv: s_map) (sk:s_map) (n:var) (*  {struct e } *): exp * var :=
        match e with
-       | Halt_c v => let (ll, n') := convert_v v sv sk n in
-                     let (ctx_v, vn) := ll in
+       | Halt_c v => let '(ctx_v, vn, n') := convert_v v sv sk n in
                          (app_ctx_f ctx_v (Eapp (2%positive) (vn::nil) ), n')
-       | Ret_c k v => let (ll1, n') := convert_v k sv sk n in
-                      let (ctx_k, kn) := ll1 in
-                      let (ll2, n'') := convert_v v sv sk n' in
-                      let (ctx_v, vn) := ll2 in
+       | Ret_c k v => let '(ctx_k, kn, n') := convert_v k sv sk n in
+                      let '(ctx_v, vn, n'') := convert_v v sv sk n' in
                        (app_ctx_f (comp_ctx_f ctx_k ctx_v) (Eapp kn (vn::nil)), n'')
        | Call_c f k v =>
-         let (ll1, n') := convert_v f sv sk n in
-         let (ctx_f, fn) := ll1 in
-         let (ll2, n'') := convert_v k sv sk n' in
-         let (ctx_k, kn) := ll2 in
-         let (ll3, n''') := convert_v v sv sk n'' in
-         let (ctx_v, vn) := ll3 in
-         let ctx_fkv := comp_ctx_f ctx_f (comp_ctx_f ctx_k ctx_v) in
-         (app_ctx_f ctx_fkv (Eapp fn (vn::kn::nil)), n''')
+         (Eapp (get_s f sv) ((get_s v sv)::(get_s k sk)::nil) , n)
          
-      | Match_c v bl =>
-        let (ll, n') := convert_v v sv sk n in
-        let (ctx_v, vn) := ll in
-        let (fl, n'') := convert_branches bl sv sk n' in
-        let (fds, tvl) := fl in
-         (app_ctx_f ctx_v (Efun fds (Ecase vn tvl)), n'')
+       | Match_c v bl =>
+         let fix convert_branches (bl:list  ((dcon * nat) * ((list NVar)* cps))) (sv: s_map) (sk:s_map) (r:var) (n:var)  (* { struct bl } *): (list (tag * exp) * var)  :=
+             (* {| n -> k, \ n+1 . (let n+2 <- \pi_1 n+1 ... n+m+1 <- \pi_m n+1 in  ce |}  *)
+             match bl with
+               | nil => ( nil, n)
+               | ((dcn, m),(xs, e))::bl' =>
+                 let (cbl, n') := convert_branches bl' sv sk r n in
+                 let (ctx_p, n'') := ctx_bind_proj r m n' in 
+                 let (names, _) := fromN n' m in
+                 let sv' := set_s_list xs names sv in
+                 let (ce, n''') :=  convert e sv' sk n'' in
+                 (((dcon_to_tag dcn), (app_ctx_f ctx_p ce))::cbl , n''') 
+             end in
+         let '(ctx_v, vn, n') := convert_v v sv sk n in
+         let '(cbls, n'') := convert_branches bl sv sk vn n' in
+         (app_ctx_f ctx_v (Ecase vn cbls), n'')
+       | Proj_c v m k =>
+         let '(ctx_v, vn, n') := convert_v v sv sk n in
+         let '(ctx_k, vk, n'') := convert_v k sv sk n' in
+         (app_ctx_f (comp_ctx_f ctx_v ctx_k) (Eproj n'' ty (N.of_nat m) vn  (Eapp vk (n''::nil))), Pos.succ n'')
        end
-(* returns converted context * new binding (usually n'-1 except for var and kvar) * next fresh name *)
-    with convert_v (v:val_c) (sv: list var) (sk: list var) (n:var) (* {| struct v |} *): (exp_ctx * var * var) :=
-           match v with
-           | Var_c m => (Hole_c, (nth (N.to_nat m) sv (1%positive)), n)   (* {| ( Econstr_c n ty var_tag ((nth (N.to_nat m) sv (1%positive))::nil)  Hole_c, Pos.succ n  ) |} *)
-           | KVar_c m => (Hole_c, (nth (N.to_nat m) sk (1%positive)), n) (* {| ( Econstr_c n ty kvar_tag ((nth (N.to_nat m) sk (1%positive))::nil) Hole_c, Pos.succ n) |} *)
-           | Lam_c e => let (e', n') := convert e (n::sv) ((Pos.succ n)::sk) (Pos.add n (2%positive)) in
-                        let fds := Fcons n' ty_fun (n::(Pos.succ n)::nil) e' Fnil in
-                        (Efun1_c fds Hole_c, n' , (Pos.succ n'))
-           | Cont_c e => let (e', n') := convert e sv (n::sk) (Pos.succ n) in
+(* returns converted context * new binding (usually n'-1 except for var and kvar) * next fresh 
+
+Ret_c M N -> 
+ let m = [[M]] in
+  let n = [[N]] in
+   App_e m n
+
+*)
+with convert_v (v:val_c) (sv: s_map) (sk: s_map) (n:var) (* { struct v }  *): (exp_ctx * var * var) :=
+       match v with
+         | Var_c m => (Hole_c, get_s m sv , n)   (* {| ( Econstr_c n ty var_tag ((nth (N.to_nat m) sv (1%positive))::nil)  Hole_c, Pos.succ n  ) |} *)
+         | KVar_c m => (Hole_c, get_s m sk, n) (* {| ( Econstr_c n ty kvar_tag ((nth (N.to_nat m) sk (1%positive))::nil) Hole_c, Pos.succ n) |} *)
+         | Lam_c x k e => let (e', n') := convert e (M.set x n sk) (M.set k (Pos.succ n) sk) (Pos.add n (2%positive)) in
+                          let fds := Fcons n' ty_fun (n::(Pos.succ n)::nil) e' Fnil in
+                          (Efun1_c fds Hole_c, n' , (Pos.succ n'))
+         | Cont_c k e => let (e', n') := convert e sv (M.set k n sk) (Pos.succ n) in
                          let fds := Fcons n' ty_con (n::nil) e' Fnil in
                          (Efun1_c fds Hole_c, n',  Pos.succ n')
-           | Con_c dcn lv => 
-             let (ll, n') := convert_v_list lv sv sk n in
-             let (lv_ctx, nl) := ll in
+         | Con_c dcn lv =>
+           let fix convert_v_list (lv :list val_c) (sv : s_map) (sk: s_map) (n: var)(* {struct lv} *): (exp_ctx * list var * var) :=
+               match lv with
+                 | nil => (Hole_c, nil, n)
+                 | v::lv' =>
+                   let '(ctx_v, vn , n') := convert_v v sv sk n in
+                   let '(ctx_lv', ln', n'') := convert_v_list lv' sv sk n' in
+                   (comp_ctx_f ctx_v ctx_lv', (vn::ln'), n'')
+                     
+               end in
+           let '(lv_ctx, nl, n') := convert_v_list lv sv sk n in
              (comp_ctx_f lv_ctx (Econstr_c n' ty (dcon_to_tag dcn) nl Hole_c), n', Pos.succ n') 
-           | Fix_c le i =>
-             let nt := fnlst_length le in (* length of the mutually recursive block *)
-             let (nfds, newn) := fromN n nt in
-             let (fds, n') := convert_fds le (nfds++sv) sk n newn in
-             (* the record should disappear during shrink reduction *) 
-             (Efun1_c fds Hole_c, (nth (N.to_nat i) nfds (1%positive)),  n' )
-           end
-(*  convert a list of simple_cps cps functions into 
- (1) a set of cps.exp functions (fundefs), each referring to a local variable (n+1), a local continuation variable (n+2) and the functions name (pushed to sv before calling convert_fds
+         | Fix_c lbt =>
+           (*  convert a list of simple_cps cps functions into 
+               (1) a set of cps.exp functions (fundefs), each referring to a local variable (n+1), a local continuation variable (n+2) and the functions name (pushed to sv before calling convert_fds
  (2) the next fresh name
 
 arguments are: 
- - fds : the list of functions to convert
+ - fds : the l ist of functions to convert
  - sv : the stack of names for local variables, including the names for the set of functions being converted
  - sk : the stack of names for continuation variables
- - currn : name of the next function to convert
+ - fnames : names given to each of the function left to convert in fds
  - n     : next available name 
 
  *)
-with  convert_fds ( fds : fnlst)  (sv: list var) (sk : list var) (currn : var) (n : var) : (fundefs * var)  :=
-  match fds with
-    | flnil => (Fnil, n)
-    | flcons e fds' =>
-      let (ce, n') := convert e (n::sv) ((Pos.succ n)::sk) ((Pos.add n 2)) in
-        let (cfds, n'') := convert_fds fds' sv sk (Pos.succ currn) n' in
-        (Fcons currn ty_fun (n::(Pos.succ n)::nil) ce cfds, n'')
-  end
-with convert_v_list (lv :vals_c) (sv : list var) (sk: list var) (n: var) : (exp_ctx * list var * var) :=
-  match lv with
-    | vcnil => (Hole_c, nil, n)
-    | vccons v lv' =>
-      let '(ctx_v, vn , n') := convert_v v sv sk n in
-      let '(ctx_lv', ln', n'') := convert_v_list lv' sv sk n' in
-      (comp_ctx_f ctx_v ctx_lv', (vn::ln'), n'')
-
-  end
-(* Translates a list of branch cps into
-   (1) a set of cps.exp functions (fundefs) representing the branches' continuation
-   (2) a list of pairs grouping the tag with their continuation name
-   (3) the next fresh name *)
-with convert_branches (bl: branches_c) (sv: list var) (sk: list var) (n:var) (* {| struct bl |} *): (fundefs * list (tag * var) * var)  :=
-    (* {| n -> k, \ n+1 . (let n+2 <- \pi_1 n+1 ... n+m+1 <- \pi_m n+1 in  ce |}  *)
-    match bl with
-      | brnil_c => (Fnil, nil, n)
-      | brcons_c dcn m e bl' =>
-        let '(cfds, cbl, n') := convert_branches bl' sv sk n in
-        let ctx_p := ctx_bind_proj' (Pos.succ n') (N.to_nat m) in 
-        let (xs, nm2) := fromN (Pos.add n' 2) (N.to_nat m) in
-        let (ce, n'') :=  convert e ((List.rev xs)++sv) sk nm2 in 
-        (Fcons n' ty_con ((Pos.succ n')::nil) (app_ctx_f ctx_p ce) cfds, ((dcon_to_tag dcn), n')::cbl ,   n'')
-    end.
+           let fix convert_fds (fds : list (var * val_c))  (sv: s_map) (sk : s_map) (fnames : list var) (n : var) (*{struct fds} *): (fundefs * var)  :=
+               match (fds, fnames) with
+                 | ((f,v)::fds', currn::fnames') =>
+                   (match v with
+                      | Lam_c x k e =>            
+                        let (ce, n') := convert e (M.set x n sv) (M.set k (Pos.succ n) sk) (Pos.add n 2%positive) in
+                        let (cfds, n'') := convert_fds fds' sv sk fnames' n' in
+                        (Fcons currn ty_fun (n::(Pos.succ n)::nil) ce cfds, n'')
+                      | _ => (Fnil, n) (* this should not happen *)
+                    end) 
+                 | (_, _) => (Fnil, n)
+               end in
+           let (fnames, fbodys) := List.split lbt in (* length of the mutually recursive block *)
+           let (nfds, newn) := fromN n (length lbt) in
+           let sv' := set_s_list fnames nfds sv in
+           let (fds, n') := convert_fds lbt sv' sk nfds newn in
+           (* the record should disappear during shrink reduction *) 
+           (Efun1_c fds (Econstr_c n' ty default_tag nfds Hole_c), n',  (Pos.succ n') )
+       end.
 
 Definition convert_top (e:cps) : exp :=
-  let (er, n) := convert e nil nil (3%positive) in er.
+  let (er, n) := convert e s_empty s_empty (3%positive) in er.
 
 
 
-(*
- {| Example e1 : expression.exp := Lam_e (Var_e 0).  |}
-{|  Example e2 : expression.exp := Lam_e (Lam_e (Var_e 1)).  |}
-{|  Eval vm_compute in cps_cvt_prog e1.  |}
-{|  Eval vm_compute in convert_top (cps_cvt_prog e1).  |}
- *)
 
-End L5toL6.
+
 

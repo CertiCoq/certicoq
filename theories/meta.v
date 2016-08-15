501e3d49be686015b468c29a16fb2d0b570f7ea0
Abhishek Anand <499314+aa755@users.noreply.github.com>
2016-08-15 01:52:51 +0000

    composition of phases and their correctness proofs
    Also, did some refractoring. Now there are 3 main type classes.
    
    1) CerticoqLanguage. A type Term can be declared an instance of it by providing a bigstep eval relation (Term -> Term -> Prop) and a well-formedness predicate (Term -> Prop).
    
    2) CerticoqTranslation : Any funtion typed Term1 -> exception Term2 can be declared an instance. If all Certicoq phases (between consecutive languages) are declared, using typeclass resolution, (translate Li Lj) will automatically denote the translation  Li -> exception Lj. Will define translateTo, such that (translateTo Lj) translates template-Coq terms to Lj
    
    3) CerticoqTranlationCorrect : to declare an instance for a translation declared above, one need to prove that the translation preserves well-formedness and big-step evaluation of well-formed terms. Such correctness proofs also automatically compose using typeclass resolution.

--- theories/meta.v
+++ theories/meta.v
@@ -2,18 +2,21 @@ Require Import Common.exceptionMonad.
 
 Require Import Common.AstCommon.
 
-Set Implicit Arguments.
+
+Class WellFormed (Term: Type) := wf : Term  -> Prop. 
+
+Generalizable Variables Src Dst Inter Term.
 
 (** A [Term] can contain an environment embedded in it. *)
-Class BigStepLanguage (Term: Type) := bigStepEval: Term -> Term -> Prop. 
+Class BigStepOpSem (Term:Type) := bigStepEval: Term -> Term -> Prop.
 
-Generalizable Variables Src Dst Term.
 
 Notation "s ⇓ t" := (bigStepEval s t) (at level 70).
 
 Require Import Coq.Unicode.Utf8.
 
-Instance liftBigStepException `{BigStepLanguage Term} : BigStepLanguage (exception Term):=
+
+Instance liftBigStepException `{BigStepOpSem Term} : BigStepOpSem (exception Term):=
 λ (s sv: exception Term),
 match (s, sv) with
 | (Ret s, Ret sv) => s ⇓ sv
@@ -21,17 +24,140 @@ match (s, sv) with
 end.
 
 
-Definition normalizes `{BigStepLanguage Term} (s:Term): Prop :=
+Class CerticoqTranslation (Src Dst : Type)
+  := translate : Src -> exception Dst.
+
+Arguments translate  Src Dst {CerticoqTranslation} s.
+
+Definition wfPreserving `{CerticoqTranslation Src Dst}
+    `{WellFormed Src} `{WellFormed Dst} : Prop := 
+  ∀ (s: Src), 
+    wf s 
+    -> match translate Src Dst s with
+      | Ret t => wf t
+      | Exc _ => False
+      end.
+
+Arguments wfPreserving Src Dst {H} {H0} {H1}.
+
+Definition bigStepPreserving `{CerticoqTranslation Src Dst}
+   `{BigStepOpSem Src} `{BigStepOpSem Dst} `{WellFormed Src}
+  :=
+   ∀ (s sv:Src), 
+    wf s 
+    -> s ⇓ sv
+    -> (translate Src Dst s) ⇓ (translate Src Dst sv).
+
+Arguments bigStepPreserving Src Dst {H} {H0} {H1} {H2}.
+
+(** A Certicoq language must have an associated bigstep operational semantics and 
+  a well formedness predicate (possibly \_.True) *)
+Class CerticoqLanguage (Term : Type) `{BigStepOpSem Term} `{WellFormed Term} := 
+{
+  (* Sensible, but not needed yet.
+  wfPreserved : forall (s v : Term), 
+    wf s
+    -> s ⇓ v
+    -> wf v
+  *)
+}.
+
+Arguments CerticoqLanguage Term {H} {H0}.
+
+Class CerticoqTranslationCorrect 
+  `{CerticoqLanguage Src} `{CerticoqLanguage Dst}
+  `{CerticoqTranslation Src Dst} := 
+{
+  certiWfPres : wfPreserving Src Dst;
+  certiBigStepPres : bigStepPreserving Src Dst;
+}.
+
+Arguments CerticoqTranslationCorrect Src {H} {H0} {H1} Dst {H2} {H3} {H4} {H5}.
+
+Global Instance composeTranslation `{CerticoqTranslation Src Inter} 
+  `{CerticoqTranslation Inter Dst} :
+  `{CerticoqTranslation Src Dst} :=
+λ s, bind (translate Src Inter s) (translate Inter Dst).
+
+
+Lemma composePreservesWf (Src Inter Dst: Type)
+  {wfi: WellFormed Inter}
+  {wfs: WellFormed Src}
+  {wft: WellFormed Dst}
+  {t1 : CerticoqTranslation Src Inter}
+  {t2 : CerticoqTranslation Inter Dst} :
+wfPreserving Src Inter
+-> wfPreserving Inter Dst
+-> wfPreserving Src Dst.
+Proof.
+  intros Hsi Hit s Hs.
+  apply Hsi in Hs.
+  unfold composeTranslation, bind.
+  unfold translate in *.
+  destruct (t1 s); [contradiction|].
+  apply Hit in Hs.
+  unfold translate in *.
+  assumption.
+Qed.
+
+
+
+Instance composeCerticoqTranslationCorrect (Src Inter Dst: Type)
+  `{CerticoqLanguage Src}
+  `{CerticoqLanguage Inter}
+  `{CerticoqLanguage Dst}
+  {t1 : CerticoqTranslation Src Inter}
+  {t2 : CerticoqTranslation Inter Dst}
+  {Ht1: CerticoqTranslationCorrect Src Inter}
+  {Ht2: CerticoqTranslationCorrect Inter Dst}
+    : CerticoqTranslationCorrect Src Dst.
+Proof.
+  destruct Ht1, Ht2.
+  constructor;[eapply composePreservesWf; eauto|].
+  intros ? ? Hwf Hev.
+  apply certiBigStepPres0 in Hev;[| assumption].
+  apply certiWfPres0 in Hwf.
+  unfold composeTranslation, translate in *.
+  destruct (t1 s), (t1 sv); compute in Hev; try contradiction.
+  apply certiBigStepPres1 in Hev;[| assumption].
+  exact Hev.
+Qed.
+
+
+
+(* ( * ) in Randy's email dated Tue, May 31, 2016 at 10:35 PM EST 
+*) 
+
+Definition bigStepReflecting `{CerticoqTranslation Src Dst}  
+   `{BigStepOpSem Src} `{BigStepOpSem Dst} (s:Src) : Prop :=
+ ∀ (td: exception Dst), 
+    (translate Src Dst s) ⇓ td
+    -> ∃ (d:Src), s ⇓ d ∧ td = translate Src Dst d.
+
+
+Arguments bigStepReflecting Src Dst {H} {H0} {H1} s.
+
+
+
+Definition normalizes `{BigStepOpSem Term} (s:Term): Prop :=
 ∃ sv, s ⇓ sv.
 
 
 (* Todo : generalize over Coq.Init.Logic.eq. *)
-Definition deterministicBigStep `{BigStepLanguage Term} :=
+Definition deterministicBigStep `{BigStepOpSem Term} :=
 ∀ (s v1 v2:Term), s ⇓ v1 -> s ⇓ v2 -> v1 = v2.
 
 Arguments deterministicBigStep Term {H}.
 
-Lemma deterministicBigStepLiftExc `{BigStepLanguage Term}:
+Definition totalTranslation `{CerticoqTranslation Src Dst} : Prop :=
+  ∀ (s:Src), 
+match translate Src Dst s with
+| Ret _ => True
+| Exc _ => False
+end.
+
+
+Lemma deterministicBigStepLiftExc `{BigStepOpSem Term}:
   deterministicBigStep Term
   -> deterministicBigStep (exception Term).
 Proof.
@@ -42,61 +168,55 @@ Proof.
   eapply Hd; eauto.
 Qed.
 
-(* TODO : BigStep semantics is not necessary to define a translation *)
-Class Translation `{BigStepLanguage Src} `{BigStepLanguage Dst}
-  := translate : Src -> exception Dst.
-
-Definition totalTranslation `{Translation Src Dst} : Prop :=
-  ∀ (s:Src), 
-match translate s with
-| Ret _ => True
-| Exc _ => False
-end.
-
-Class BigStepPreserving `{Translation Src Dst} (precondition : Src -> Prop)
-  :=
-  preservesEval  : ∀ (s sv:Src), 
-    precondition s 
-    -> s ⇓ sv
-    -> (translate s) ⇓ (translate sv)
-. 
-
-(* ( * ) in Randy's email dated Tue, May 31, 2016 at 10:35 PM EST 
-*) 
-
-Definition bigStepReflecting `{Translation Src Dst} (precondition : Src -> Prop) (s:Src) : Prop :=
- ∀ (td: exception Dst), 
-    (translate s) ⇓ td
-    -> ∃ (d:Src), s ⇓ d ∧ td = translate d. 
-
-
-Arguments BigStepPreserving Src {H} Dst {H0} {H1} precondition.
-Arguments bigStepReflecting Src {H} Dst {H0} {H1} precondition s.
-
-
-Generalizable Variable precondition.
-
-Lemma bigStepPreservingReflecting  `{BigStepPreserving Src Dst precondition}: 
+Lemma bigStepPreservingReflecting 
+  `{CerticoqTranslation Src Dst}
+  `{CerticoqLanguage Src}
+  `{BigStepOpSem Dst}
+  `{@bigStepPreserving Src Dst _ _ _ _ } :
   (deterministicBigStep Dst)
   -> ∀ (s:Src), 
-    precondition s
+    wf s
     -> normalizes s
-    -> bigStepReflecting Src Dst precondition s.
+    -> bigStepReflecting Src Dst s.
 Proof.
   intros Hd ? Hp Hn ? Ht.
   destruct Hn as [d  Hn].
   exists d. split;[assumption|].
   apply deterministicBigStepLiftExc in Hd.
-  eauto.
-(*
-(* info eauto : *)
+  info_eauto.
+(* info eauto : 
 eapply Hd.
  exact Ht.
- apply H2.
+ apply preservesEval0.
   exact Hp.
   exact Hn.
 *)
 Qed.
 
-  
+
+Fixpoint FunctionType (L:list Type) (R:Type): Type :=
+match L with
+| nil => R
+| cons H T => H -> (FunctionType T R)
+end.
+
+Fixpoint excFunction (L:list Type) (R:Type) (err: String.string) {struct L}: 
+  (FunctionType (List.map exception L) (exception R)) :=
+match L return (FunctionType (List.map exception L) (exception R)) with
+| nil => Exc err
+| cons H T => λ (he : exception H), (excFunction T R err)
+end.
+
+
+Fixpoint liftExeption (L:list Type) (R:Type) 
+  (f: FunctionType L R) {struct L}: (FunctionType (List.map exception L) (exception R)) :=
+match L 
+  return (FunctionType L R -> FunctionType (List.map exception L) (exception R)) with
+| nil => λ f, Ret f
+| cons H T => fun ff he => 
+    match he with
+    | Exc err => excFunction T R err
+    | Ret h =>  liftExeption T R (ff h)
+    end
+end f.
 

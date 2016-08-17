c9cf64a9a8679a328860391b5ab9e160509abfd4
Abhishek Anand <499314+aa755@users.noreply.github.com>
2016-08-17 19:34:10 +0000

    big step eval is now heterogenous, as suggested by Zoe.
    Existing instances were automatically lifted using typeclass resolution, without any changes to the sourcecode.
    Fixed some instances for L6. Checked that translateTo works upto L6 -- the last line in theories/allInstances.v

--- theories/L6_PCPS/instances.v
+++ theories/L6_PCPS/instances.v
@@ -5,29 +5,34 @@ Require Import cps_util.
 
 Require Import certiClasses.
 Require Import Common.Common.
+Require Import L6.eval.
+(* 
+Let L6env : Type := prims * cEnv * env.
 
-(* Fix. L6 bigstep eval (L6.eval.bigstep_e) is strange it is a hetergenous relation of type
-  [env -> cps.exp -> val -> nat].
-  Perhaps the L6 term type should be (env * (cps.exp + cps.val)) 
-  bigstep_e counts costs, which is not a problem because we can existentially quantify 
-  over the count.
 *)
-Instance bigStepOpSemL3Term : BigStepOpSem cps.exp :=
-  fun _ _ => True.
+
+
+Require Import Coq.Unicode.Utf8.
+
+(* Should p c e be particular values? The translation from L5a doesn't produce
+  these values. If it did, we could make the terms contain this information, as in L3 *)
+Instance bigStepOpSemL3Term : BigStepHetero cps.exp cps.val :=
+  λ s v,
+    ∀ p c e, 
+    ∃ (n:nat), (L6.eval.bstep_e p c e s v n).
 
 (* Fix *)
-Instance WfL3Term : WellFormed cps.exp :=
+Instance WfL3Term : WellFormed (*L6env*) cps.exp :=
   fun p  => True .
 
-Instance certiL6 : CerticoqLanguage cps.exp := {}.
+Instance certiL6 : CerticoqLanguage (*L6env*)cps.exp := {}.
 
 Require Import L4.instances.
 Require Import L5_to_L6.
 
 Instance certiL5a_t0_L6: 
-  CerticoqTranslation L5a.cps cps.exp :=
-fun x => Ret (convert_top x).
-
+  CerticoqTotalTranslation L5a.val_c cps.exp := 
+  fun v => convert_top (L5a.Halt_c v).
 
 
 

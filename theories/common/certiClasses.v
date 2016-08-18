06e5c33ebe7cb3675f97f5cc0a4b9088a82a98f4
Abhishek Anand <499314+aa755@users.noreply.github.com>
2016-08-18 20:10:58 +0000

    new correctness property of phases, proof of its composition.
    The CerticoqLanguage2 class requires the following : a type of values, which may be the same as the type of terms, well formedness predicate on terms, a big step eval relation from terms to values, a function to observe the head constructor of a value (if any), a function to observe constructor arguments of a value (if any).
    
    The class CerticoqTranslationCorrect2 requires a proof of the following
    1) the translation preserves well-formedness
    2)  ∀ (s:Src) (sv: SrcValue),
        wf s
        -> (s ⇓ sv)
        -> ∃ (dv: DstValue), (translate Src Dst s) ⇓ (Ret dv) ∧  sv ~ dv.
    
    ~ is coinductively defined using the observation functions mentioned above.
    
    The instances of this class compose -- a proof of correctness of Li to Lk via Lj can be automatically obtained by proofs fof Li to Lj and Lj to Lk.
    See common.certiClasses.composeCerticoqTranslationCorrect2

--- theories/common/certiClasses.v
+++ theories/common/certiClasses.v
@@ -91,51 +91,13 @@ Definition bigStepPreserving `{CerticoqTranslation Src Dst}
 
 Arguments bigStepPreserving Src Dst {H} {SrcValue} {DstValue} {H0} {H1} {H2} {H3}.
 
-(* We fix one type of observations of the head of a value. Typically,
-these would be the tags of constructors of (co-) inductive types.
-If a value is not a constructor applied to some args, e.g. a lambda,
-the observation should be None*)
-
-Definition Observation :Set := option (inductive * nat).
-
-Class ObserveHead (Value:Type) := observeHead: Value -> Observation.
-
-(* observe subterms for a constructor. return [] if the value is not a constructor. *)
-Class ObserveSubterms (Value:Type) := observeSubterms: Value -> list Value.
-
-Require Import List.
-(* Coinductive, in case we add support for Coq's coinductive types lateron *)
-CoInductive obsEqual
-   `{ObserveHead SrcValue} `{ObserveSubterms SrcValue} 
-   `{ObserveHead DstValue} `{ObserveSubterms DstValue} : SrcValue -> DstValue -> Prop :=
-| sameObs : forall (s : SrcValue) (d : DstValue),
-    observeHead s = observeHead d
-    -> (let ls := (observeSubterms s) in
-       let ld := (observeSubterms d) in
-       length ls = length ld /\
-       forall n:nat, obsEqual (nth n ls s)  (nth n ld d))
-    -> obsEqual s d.
-
-Notation "s ~ t" := (obsEqual s t) (at level 65).
-
-(* Similar to what Zoe suggested on 	Wed, Aug 17, 2016 at 8:57 AM *)
-Definition obsPreserving 
-  `{CerticoqTranslation Src Dst} 
-   `{BigStepHetero Src SrcValue} `{BigStepHetero Dst DstValue} `{WellFormed Src}
-   `{ObserveHead SrcValue} `{ObserveSubterms SrcValue} 
-   `{ObserveHead DstValue} `{ObserveSubterms DstValue}
-  :=
-   ∀ (s:Src) (sv: SrcValue), 
-    wf s 
-    -> (s ⇓ sv)
-    -> ∃ (dv: DstValue), (translate Src Dst s) ⇓ (Ret dv) ∧  sv ~ dv.
 
 Class CerticoqTranslationCorrect 
   `{CerticoqLanguage Src SrcValue} `{CerticoqLanguage Dst DstValue}
   `{CerticoqTranslation Src Dst} `{CerticoqTranslation SrcValue DstValue} := 
 {
   certiWfPres : wfPreserving Src Dst;
-  certiBigStepPres : bigStepPreserving Src Dst;
+  certiBigStepPres : bigStepPreserving Src Dst; (* consider  [obsPreserving] below*)
 }.
 
 Arguments CerticoqTranslationCorrect 
@@ -191,6 +153,165 @@ Proof.
   exact Hev.
 Qed.
 
+(* We fix one type of observations of the head of a value. Typically,
+these would be the tags of constructors of (co-)inductive types.
+If a value is not a constructor applied to some args, e.g. a lambda,
+the observation should be None.
+
+This means, that \x.1 ~ \x.2 , which is unfortunate. It would be especially bad if,
+although unlikely,
+the IO monad had an operation that takes a function of type nat -> string, e.g. to
+indicate the string to be sent at the nth tick.
+
+Can ¬ (\x.1 ~ \x.2) still be implied using the existing assumptions in 
+  [CerticoqTranslationCorrect2]?
+
+Should we allow some computation when making observation, especially on subterms?
+When we add cofix/coinductives/effects, this may become necessary.
+*)
+
+Definition Observation :Set := option (inductive * nat).
+
+Class ObserveHead (Value:Type) := observeHead: Value -> Observation.
+
+(* observe subterms for a constructor. return [] if the value is not a constructor. *)
+Class ObserveSubterms (Value:Type) := observeSubterms: Value -> list Value.
+
+Require Import List.
+(* Coinductive, in case we add support for Coq's coinductive types lateron *)
+CoInductive obsEqual
+   `{ObserveHead SrcValue} `{ObserveSubterms SrcValue} 
+   `{ObserveHead DstValue} `{ObserveSubterms DstValue} : SrcValue -> DstValue -> Prop :=
+| sameObs : forall (s : SrcValue) (d : DstValue),
+    observeHead s = observeHead d
+    -> (let ls := (observeSubterms s) in
+       let ld := (observeSubterms d) in
+       length ls = length ld /\
+       forall n:nat, obsEqual (nth n ls s)  (nth n ld d))
+    -> obsEqual s d.
+
+Notation "s ~ t" := (obsEqual s t) (at level 65).
+
+(* Similar to what Zoe suggested on 	Wed, Aug 17, 2016 at 8:57 AM *)
+Definition obsPreserving 
+  `{CerticoqTranslation Src Dst} 
+   `{BigStepHetero Src SrcValue} `{BigStepHetero Dst DstValue} `{WellFormed Src}
+   `{ObserveHead SrcValue} `{ObserveSubterms SrcValue} 
+   `{ObserveHead DstValue} `{ObserveSubterms DstValue}
+  :=
+   ∀ (s:Src) (sv: SrcValue), 
+    wf s 
+    -> (s ⇓ sv)
+    -> ∃ (dv: DstValue), (translate Src Dst s) ⇓ (Ret dv) ∧  sv ~ dv.
+
+Arguments obsPreserving Src Dst {H} {SrcValue} {H0} {DstValue} {H1} {H2} {H3} {H4} {H5} {H6}.
+
+Class CerticoqLanguage2 `{BigStepHetero Term Value} `{WellFormed Term} 
+   `{ObserveHead Value} `{ObserveSubterms Value} 
+:= 
+{
+  (* Sensible, but not needed yet.
+  wfPreserved : forall (s v : Term), 
+    wf s
+    -> s ⇓ v
+    -> wf v
+  *)
+}.
+
+(*
+Arguments CerticoqLanguage2 Term {Value} {H} {H0} {H1} {H2}.
+
+Class CerticoqTranslationCorrect2 Src SrcValue Dst DstValue S1 S2 S3 S4 D1 D2 D3 D4
+  `{@CerticoqLanguage2 Src SrcValue S1 S2 S3 S4} 
+  `{@CerticoqLanguage2 Dst DstValue D1 D2 D3 D4}
+  `{CerticoqTranslation Src Dst} := 
+{
+  certiWfPres2 : wfPreserving Src Dst;
+  obsePres : obsPreserving Src Dst;
+}.
+*)
+
+
+(* later in this file, we make [Value] an implicit argument. Having it be explicit
+  here cuts down a lot of verbosity (by using `, which doesn't work with @) 
+  in some definitions below.
+ *)
+Arguments CerticoqLanguage2 Term Value {H} {H0} {H1} {H2}.
+
+Class CerticoqTranslationCorrect2
+  `{CerticoqLanguage2 Src SrcValue} 
+  `{CerticoqLanguage2 Dst DstValue}
+  `{CerticoqTranslation Src Dst} := 
+{
+  certiWfPres2 : wfPreserving Src Dst;
+  obsePres : obsPreserving Src Dst;
+}.
+
+Arguments CerticoqTranslationCorrect2
+  Src {SrcValue} {H} {H0} {H1} {H2} {H3} Dst {DstValue} {H4} {H5} {H6} {H7} {H8} {H9}.
+
+(*
+Does not have a any theory of heterigeneous relations. Niether does MathClasses.
+Require Import Coq.Classes.RelationClasses.
+*)
+
+
+Lemma obsEqualTrns
+   `{ObserveHead SrcValue} `{ObserveSubterms SrcValue} 
+   `{ObserveHead InterValue} `{ObserveSubterms InterValue} 
+   `{ObserveHead DstValue} `{ObserveSubterms DstValue} :
+   forall   (s : SrcValue) (i : InterValue) (d : DstValue),
+  s ~ i
+  -> i ~ d 
+  -> s ~ d.
+Proof.
+  cofix.
+  intros ? ? ? Ha Hb.
+  inversion Ha as [ss is Hah Has]. subst. clear Ha.
+  inversion Hb as [is ds Hbh Hbs]. subst. clear Hb.
+  constructor;[congruence|].
+  simpl in *. destruct Has as [Hasl Has].
+  destruct Hbs as [Hbsl Hbs].
+  split; [congruence|].
+  eauto.
+(* info eauto : 
+intro.
+eapply obsEqualTrns.
+ apply Has.
+ apply Hbs.
+*)
+Qed.
+
+Instance composeCerticoqTranslationCorrect2
+  `{CerticoqLanguage2 Src SrcValue}
+  `{CerticoqLanguage2 Inter InterValue}
+  `{CerticoqLanguage2 Dst DstValue}
+  {t1 : CerticoqTranslation Src Inter}
+  {t2 : CerticoqTranslation Inter Dst}
+(* we don't anymore need a translation for the value type, although typically Src=SrcValue*)
+  {Ht1: CerticoqTranslationCorrect2 Src Inter}
+  {Ht2: CerticoqTranslationCorrect2 Inter Dst}
+    : CerticoqTranslationCorrect2 Src Dst.
+Proof.
+  destruct Ht1, Ht2.
+  constructor;[eapply composePreservesWf; eauto; fail|].
+  intros ? ? Hwf Hev.
+  apply obsePres0 in Hev;[| assumption].
+  apply certiWfPres3 in Hwf.
+  unfold composeTranslation, translate in *.
+  destruct (t1 s); compute in Hev; try contradiction.
+  destruct Hev as [iv Hev].
+  destruct Hev as [Hev Hoeq].
+  apply obsePres1 in Hev;[| assumption].
+  destruct Hev as [dv Hc].
+  exists dv. compute.
+  compute in Hc. split;[tauto|].
+  apply proj2 in Hc. revert Hc Hoeq. clear.
+  intros ? ?.
+  eapply obsEqualTrns with (i:=iv); eauto.
+Qed.
+
+
 
 Instance  BigStepOpWEnv (Term:Set) (ev: (environ Term) -> Term -> Term -> Prop) :
   BigStepOpSem (Program Term) :=
@@ -226,36 +347,6 @@ Proof.
 Qed.
 
 
-(* A possible replacement for [CerticoqTranslationCorrect] 
-Class CerticoqTranslationCorrect2 
-  `{CerticoqLanguage Src} `{CerticoqLanguage Dst}
-  `{CerticoqTranslation Src Dst} := 
-{
-  certiWfPres2 : wfPreserving Src Dst;
-  certiBigStepPres2 : bigStepPreservingWeaker Src Dst;
-}.
-
-Arguments CerticoqTranslationCorrect2 Src {H} {H0} {H1} Dst {H2} {H3} {H4} {H5}.
-
-
-Instance composeCerticoqTranslationCorrect2 (Src Inter Dst: Type)
-  `{CerticoqLanguage Src}
-  `{CerticoqLanguage Inter}
-  `{CerticoqLanguage Dst}
-  {t1 : CerticoqTranslation Src Inter}
-  {t2 : CerticoqTranslation Inter Dst}
-  {Ht1: CerticoqTranslationCorrect2 Src Inter}
-  {Ht2: CerticoqTranslationCorrect2 Inter Dst}
-    : CerticoqTranslationCorrect2 Src Dst.
-Proof.
-  destruct Ht1, Ht2.
-  constructor;[eapply composePreservesWf; eauto|].
-  intros ? ? ? Hwf Hev.
-  specialize (fun ti => certiBigStepPres3 _ _ ti Hwf Hev).
-
-*)
-
-
 (* ( * ) in Randy's email dated Tue, May 31, 2016 at 10:35 PM EST 
 *) 
 
@@ -286,7 +377,7 @@ Proof.
   destruct Hn as [d  Hn].
   exists d. split;[assumption|].
   apply deterministicBigStepLiftExc in Hd.
-  info_eauto.
+  eauto.
 (* info eauto : 
 eapply Hd.
  exact Ht.

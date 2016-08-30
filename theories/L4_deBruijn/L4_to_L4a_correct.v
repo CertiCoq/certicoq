674eba0c2695b34ddab06e1e660d00f9da6908ac
Abhishek Anand <499314+aa755@users.noreply.github.com>
2016-08-30 04:24:52 +0000

    WIP : eval commutes with L4 to L4a
    4 admits remaining.
    3 are about the 3 substitution functions used in L4.eval.
    1 is about the mismatch in how the iota reduction picks the matching branch

--- theories/L4_deBruijn/L4_to_L4a_correct.v
+++ theories/L4_deBruijn/L4_to_L4a_correct.v
@@ -138,7 +138,6 @@ Proof using.
 Qed.
 
 
-
 Lemma L4_to_L4a_fvars: 
   (forall n (s : exp),
     L4.expression.exp_wf n s 
@@ -158,11 +157,10 @@ Lemma L4_to_L4a_fvars:
  ∧
   (forall n (s : branches_e),
     L4.expression.branches_wf n s 
-    -> forall v, List.In v (flat_map (free_vars_bterm ∘ snd)  (translateb mkVar n s)) -> 
-        (Npos (Pos.div2 v) < N.succ n)%N /\ varClass v = true (* user variable *))
-.
+    -> forall v, List.In v (flat_map (free_vars_bterm ∘ snd)  (translatelb mkVar n s)) -> 
+        (Npos (Pos.div2 v) < N.succ n)%N /\ varClass v = true (* user variable *)).
 Proof using.
-  apply my_exp_wf_ind; try (simpl; tauto);
+  apply my_exp_wf_ind; try (simpl; tauto); [ | | | | | | | | | ];
   intros ? ?.
 (* Var_e *)
 - intros Hwf ? Hin. simpl in Hin. rewrite or_false_r in Hin.
@@ -173,7 +171,7 @@ Proof using.
 
 (* Lam_e *)
 - intros Hwf Hind ? Hin.
-  simpl in Hin. fold (L4_to_L4a.translate mkVar (N.succ i)) in Hin.
+  simpl in Hin.
   rewrite list.in_app_iff in Hin.
   rewrite or_false_r in Hin.
   apply list.in_remove in Hin.
@@ -196,7 +194,6 @@ Proof using.
 
 (* App_e *)
 - intros ? ? H1ind H1wf H2ind ? Hin. simpl in Hin.
-  fold (L4_to_L4a.translate mkVar i) in Hin.
   repeat (rewrite list.in_app_iff in Hin).
   rewrite or_false_r in Hin.
   dorn Hin; eauto.
@@ -204,15 +201,12 @@ Proof using.
 (* Con_e *)
 - intros ? Hwf Hind ? Hin.
   simpl in Hin.
-  fold (L4_to_L4a.translatel mkVar i) in Hin.
   rewrite flat_map_bterm_nil in Hin.
   eauto.
 
 (* Match_e *)
 - intros ? ? Hwf Hind ? Hindb ? Hin.
   simpl in Hin.
-  fold (L4_to_L4a.translateb mkVar i) in Hin.
-  fold (L4_to_L4a.translate mkVar i) in Hin.
   apply list.in_app_iff in Hin.
   rewrite list.flat_map_map in Hin.
   dorn Hin; eauto.
@@ -221,8 +215,6 @@ Proof using.
 (* Let_e *)
 - intros ? Hwf Hind H2wf H2ind ? Hin.
   simpl in Hin.
-  fold (L4_to_L4a.translate mkVar (N.succ i)) in Hin.
-  fold (L4_to_L4a.translate mkVar i) in Hin.
   repeat rewrite list.in_app_iff in Hin.
   simpl in Hin.
   rewrite or_false_r in Hin.
@@ -250,7 +242,6 @@ Proof using.
 
 - intros ? Hwf Hind ? Hin.
   simpl in Hin.
-  fold (L4_to_L4a.translatef mkVar (i + (efnlst_length es))%N) in Hin.
   rewrite N.add_comm in Hin.
   rewrite flat_map_map in Hin.
   unfold compose in Hin.
@@ -268,18 +259,12 @@ Proof using.
   clear Hvc. lia.
 
 - intros ? Hwf Hind H2wf H2ind ? Hin. simpl in Hin.
-  fold (L4_to_L4a.translate mkVar i) in Hin.
-  fold (L4_to_L4a.translatel mkVar i) in Hin.
   apply in_app_iff in Hin.
   dorn Hin; eauto.
 - intros ? Hwf Hind H2wf H2ind ? Hin. simpl in Hin.
-  fold (L4_to_L4a.translate mkVar i) in Hin.
-  fold (L4_to_L4a.translatef mkVar i) in Hin.
   apply in_app_iff in Hin.
   dorn Hin; eauto.
 - intros ? ? ? Hwf Hind  H2wf H2ind ? Hin. simpl in Hin.
-  fold (L4_to_L4a.translate mkVar (i+n)) in Hin.
-  fold (L4_to_L4a.translateb mkVar i) in Hin.
   apply in_app_iff in Hin. 
   dorn Hin; [ clear H2ind | ]; eauto. unfold compose in Hin.
   simpl in Hin.
@@ -293,7 +278,337 @@ Proof using.
   setoid_rewrite in_seq_Nplus in Hin. lia.
 Qed.
 
+Lemma L4_to_L4a_closed: 
+  forall (s : exp),
+    L4.expression.exp_wf 0 s 
+    -> closed (L4_to_L4a 0 s).
+Proof using.
+  intros.
+  pose proof L4_to_L4a_fvars. repnd. clear H2 H3 H0.
+  unfold closed.
+  apply' H1  H.
+  clear H1.
+  apply null_iff_nil.
+  intros ? Hin.
+  apply_clear H in Hin. lia.
+Qed.
 
+Require Import Morphisms.
+
+Definition lsbst (es:list exp) (e : exp) : exp :=
+  fold_left (* _right ? Matthieu's email Thu, Aug 11, 2016 at 3:03 PM EST *)
+      (fun e esi => e{0 ::= esi}) es e. (* generalize over 0? *)
+
+Open Scope N_scope.
+Require Import RandyPrelude.
+
+
+Lemma succ_sub_1 : forall n,  N.succ n - 1 = n.
+Proof using.
+intros. lia.
+Qed.
+
+Lemma mkVarInj : forall a b, mkVar a = mkVar b -> a = b.
+Proof using.
+  intros ? ? Heq.
+  apply (f_equal (fun x =>  N.pos (Pos.div2 x))) in Heq.
+  do 2 rewrite mkVarDiv in Heq. lia.
+Qed.
+
+
+Lemma L4_to_L4a_ssubst:
+    forall (ls : list exp),
+    let sub n := combine 
+                (map mkVar (rev (L4_to_L4a.seq N.succ n (length ls))))
+                (map (L4_to_L4a 0) ls) in
+ lforall (exp_wf 0) ls
+  ->
+ (forall n (e : exp),
+    L4.expression.exp_wf n e
+    -> L4_to_L4a n (lsbst ls e) = ssubst (L4_to_L4a n e) (sub n))
+ ∧
+  (forall n (s : exps),
+    L4.expression.exps_wf n s 
+    -> True)
+ ∧
+  (forall n (s : efnlst),
+    L4.expression.efnlst_wf n s 
+    -> True)
+ ∧
+  (forall n (s : branches_e),
+    L4.expression.branches_wf n s 
+    -> True).
+Proof using.
+  intros ? ? Hlwf. unfold sub. clear sub.
+  apply my_exp_wf_ind; try (simpl; tauto); [ | | | | | | | ];
+  intros ? ?.
+(* Var_e *)
+- intros Hlt. simpl. revert dependent i. revert j.
+  induction ls as [ |s ls]; intros ? ? ?;[ refl | ].
+  simpl.
+  case_if as Hj;[ lia | ]. clear Hj.
+  rename i into n.
+  destruct n;[ lia | ].
+  rewrite <- N.sub_add_distr, N.add_1_r.
+(*  rewrite (@rev_unit _ _ (N.pos p)).
+SearchAbout rev.
+  rewrite Nat2N.inj_succ in *.
+  destruct (N.zero_or_succ j) as [ Hd | Hd]; subst.
+  + simpl. rewrite OrdersEx.N_as_OT.sub_0_r. simpl.
+    rewrite <- beq_var_refl.
+    SearchAbout exp_wf sbstitute.
+    pose proof (proj1 (sbst_closed_id s) 0).
+    admit.
+  + destruct Hd as [j' Hd]. subst. rename j' into j.
+    rewrite N.compare_lt_iff in H0.
+    rewrite cons_as_app in Hlwf.
+    apply lforallApp in Hlwf. repnd.
+    rewrite <- N.succ_lt_mono in H0.
+    specialize (IHls Hlwf _ _ H0).
+    rewrite Nat2N.id in IHls.
+    specialize (IHls eq_refl).
+    cases_if; [ lia | ].
+    rewrite succ_sub_1. rewrite IHls.
+    rewrite <- N.sub_add_distr, N.add_1_r.
+    rewrite beq_deq.
+    cases_if as Hd;[provefalse |  refl].
+    apply mkVarInj in Hd. lia.
+
+- intros Hwf Hl.
+Qed.
+*)
+Abort.
+
+
+Lemma translatel_len : forall A f n es, 
+  length (@translatel A f n es) = N.to_nat (exps_length es).
+Proof using.
+  induction es; auto.
+  simpl. rewrite IHes.
+  rewrite <- N2Nat.inj_succ.
+  rewrite <- N.add_1_l.
+  refl.
+Qed.
+
+(* exact same as above *)
+Lemma translatef_len : forall A f n es, 
+  length (@translatef A f n es) = N.to_nat (efnlst_length es).
+Proof using.
+  induction es; auto.
+  simpl. rewrite IHes.
+  rewrite <- N2Nat.inj_succ.
+  rewrite <- N.add_1_l.
+  refl.
+Qed.
+
+
+Lemma gseq_length A (f:A->A) n x : length (L4_to_L4a.seq f x n) = n.
+Proof using.
+  intros.
+  rewrite seq_spec, map_length, seq_length. refl.
+Qed.
+
+Hint Rewrite translatef_len translatel_len gseq_length: list.
+
+SearchAbout L4_to_L4a.seq.
+
+Lemma translate_find_branch : ∀ d vs bs e,
+expression.find_branch d (exps_length vs) bs = Some e
+-> find_branch d (N.to_nat (exps_length vs)) (translatelb mkVar 0 bs) = 
+   Some (snd (translateb mkVar 0 L4_to_L4a.translate d  (exps_length vs) e)).
+Proof using.
+  intros ? ? ? ? Hfb. unfold find_branch.
+  induction bs; auto;[ inverts Hfb | ]; simpl in *.
+  unfold num_bvars. simpl.
+  rewrite seq_spec, map_length. rewrite map_length.
+  rewrite seq_length.
+  cases_if as Hd.
+- apply Bool.andb_true_iff in Hd.
+  setoid_rewrite DecidableClass.Decidable_spec in Hd.
+  repnd.
+  apply beq_nat_true in Hd. symmetry in Hd0. rewrite Hd0 in *.
+  clear Hd0. clear d0.
+  simpl in *.
+  apply N2Nat.inj_iff in Hd.
+  cases_if; try tauto.
+  subst.
+  cases_if; try tauto.
+  inverts Hfb. refl.
+- 
+Admitted.
+
+Lemma exps_skipn0 es : exps_skipn 0 es = es.
+Proof using.
+  destruct es; auto.
+Qed.
+
+Local Lemma eval_eq_l : forall a a' b, 
+  eval a b
+  -> a=a'
+  -> eval a' b.
+Proof using.
+  intros; subst; auto.
+Qed.
+
+(* move to expression.v *)
+Lemma evals_length es vs: 
+evals es vs
+-> exps_length es = exps_length vs.
+Proof using.
+ intro H. induction H; auto.
+ simpl. rewrite IHevals. refl.
+Qed.
+
+Lemma sbst_translate_comm e1 v2:
+exp_wf 1 e1
+-> exp_wf 0 v2
+-> L4_to_L4a.translate mkVar 0 (sbst v2 0 e1)
+   = ssubst (L4_to_L4a.translate mkVar 1 e1) [(mkVar 0, L4_to_L4a.translate mkVar 0 v2)].
+Admitted.
+
+Lemma sbst_list_translate_comm e vs:
+exp_wf (exps_length vs) e
+-> exps_wf 0 vs
+-> L4_to_L4a.translate mkVar 0 (sbst_list e vs) =
+   ssubst (L4_to_L4a.translate mkVar (exps_length vs) e)
+    (combine (map mkVar (L4_to_L4a.seq N.succ 0 (N.to_nat (exps_length vs))))
+    (translatel mkVar 0 vs)).
+Admitted.
+
+Lemma sbst_fix_translate_comm  e es:
+exp_wf (efnlst_length es) e
+-> efnlst_wf (efnlst_length es) es
+-> L4_to_L4a.translate mkVar 0 (sbst_fix es e) =
+ssubst (L4_to_L4a (efnlst_length es) e)
+  (combine (map mkVar (L4_to_L4a.seq N.succ 0 (N.to_nat (efnlst_length es))))
+     (map
+        (Fix_e'
+           (map (bterm (map mkVar (L4_to_L4a.seq N.succ 0 (N.to_nat (efnlst_length es)))))
+              (translatef mkVar (efnlst_length es) es)))
+        (seq 0 (N.to_nat (efnlst_length es))))).
+(* can sbst_fix implemented using sbst_list, and then can this be a corollary of 
+sbst_list_translate_comm above *)
+Admitted.
+
+
+Lemma enthopt_translate max e n es:
+enthopt (N.to_nat n) es = Some e
+-> select (N.to_nat n) (translatef mkVar max es) 
+   = Some (L4_to_L4a max e).
+Proof using.
+ revert es.
+ induction n using N.peano_rect; simpl; destruct es as [ | he es]; simpl;
+ try rewrite N2Nat.inj_succ;
+ intros Hnth; try  invertsn Hnth; eauto.
+Qed.
+
+
+Lemma preserves_eval :
+(∀ (e v : exp),expression.eval e v 
+  -> exp_wf 0 e -> eval (L4_to_L4a 0 e) (L4_to_L4a 0 v))
+∧ (∀ (es vs : exps), evals es vs
+    -> exps_wf 0 es
+    -> forall e v, 
+       In (e,v) (combine (translatel mkVar 0 es) (translatel mkVar 0 vs)) 
+       -> eval e v).
+Proof using.
+  apply my_eval_ind; [ | | | | | | | | | ]; unfold L4_to_L4a.
+- intros? _. simpl. constructor.
+(* beta *)
+- intros ? ? ? ? ? H1s H1t H2s H2t H3s H3t Hwf.
+  simpl in *.
+  inverts Hwf.
+  econstructor; eauto.
+  clear H1t H2t.
+  eapply eval_preserves_wf in H1s; eauto.
+  eapply eval_preserves_wf in H2s; eauto.
+  clear H2 H3.
+  inverts H1s as H1s.
+  eapply eval_eq_l; eauto.
+  simpl. unfold subst. simpl in H1s.
+  apply sbst_translate_comm; auto.
+
+- intros ? ? ? H1s H1t Hwf.
+  simpl.
+  inverts Hwf.
+  econstructor; eauto.
+  do 2 rewrite translatel_len. f_equal.
+  apply evals_length; auto.
+
+(* zeta *)
+- intros ? ? ? ? H1s H1t H2s H2t Hwf.
+  inverts Hwf. simpl.
+  econstructor; eauto.
+  clear H1t.
+  eapply eval_preserves_wf in H1s; eauto.
+  eapply eval_eq_l; eauto.
+  simpl. unfold subst.
+  eapply sbst_translate_comm; eauto.
+
+(* iota *)
+- intros ? ? ? ? ? ? ? H1s H1t Hfb H2s H2t Hwf.
+  simpl in *.
+  inverts Hwf.
+  assert (p=0) by admit. (* fix either L4.eval or L4a.eval,L5.eval *)
+  subst.
+  rewrite exps_skipn0 in *.
+  econstructor; eauto.
+  rewrite translatel_len.
+  eapply translate_find_branch; eauto.
+  simpl. unfold apply_bterm. simpl.
+  eapply find_branch_preserves_wf in Hfb; eauto.
+  eapply eval_preserves_wf in H1s; eauto.
+  inverts H1s.
+  eapply eval_eq_l; eauto using subst_list_preserves_exp_wf.
+  rewrite N.add_0_r in Hfb.
+  apply sbst_list_translate_comm; auto.
+
+(* eval_Fix_e  *)
+- intros.  econstructor; eauto.
+
+(*  eval_FixApp_e *)
+- intros ? ? ? ? ? ? ? H1s H1t  H2s H2t Hnth H3s H3t Hwf.
+  simpl in *.
+  inverts Hwf.
+  pose proof Hnth as Hnthb.
+  eapply enthopt_translate with (max:= efnlst_length es) in Hnth; eauto.
+  eapply eval_FixApp_e; eauto;
+    [ apply select_map; apply Hnth 
+      | 
+      | unfold num_bvars; simpl; autorewrite with list;refl].
+  
+  autorewrite with list.
+  unfold apply_bterm. simpl.
+  eapply eval_preserves_wf in H1s; eauto.
+  invertsn H1s.
+  rewrite N.add_0_r in H1s.
+  eapply eval_preserves_wf in H2s; eauto.
+  eapply nthopt_preserves_wf in Hnthb; eauto.
+  eapply eval_eq_l; [ apply H3t; constructor; eauto using sbst_fix_preserves_wf; fail | ].
+  f_equal.
+  apply sbst_fix_translate_comm; auto.
+
+- intros. simpl. constructor; auto. cpx.
+
+- simpl. tauto.
+
+- intros ? ? ? ? H1s H1t H2s H2t Hwf ? ? Hin. inverts Hwf. 
+  simpl in Hin. dorn Hin; eauto;[].
+  inverts Hin; auto; fail.
+
+Admitted.
+
+  
+
+(*
+subst (L4_to_L4a.translate mkVar 1 t) (mkVar 0) (L4_to_L4a.translate mkVar 0 v2) =
+L4_to_L4a.translate mkVar 0 (sbst v2 0 t)
+*)
+  
+  
+
+
+  
 Lemma L4_to_L4a_preserves_wf : 
   certiClasses.wfPreserving (cTerm certiL4) (cTerm certiL4a).
 Proof.
@@ -305,17 +620,18 @@ Abort.
 Print Nat.even.
 Fixpoint isEven (n:nat) : bool := 
 match n with
-| 0 => true
+| O => true
 | S n' => isOdd n' (* && isEven n' *)
 end
 with isOdd (n:nat) : bool :=
 match n with
-| 0 => false
+| O => false
 | S n' => isEven n' (* && isOdd n' *)
 end.
 
 Quote Recursively Definition isEven3 := (isEven 3).
 Require Import Arith.
+
 Quote Recursively Definition even3 := (Nat.even 3).
 
 (* Move to an earlier file because it is more generally useful *)
@@ -351,6 +667,13 @@ Eval compute in (DecidableClass.decide (2=3))%positive.
 
 Require Import L4.expression.
 Require Import Common.Common.
+
+
+Eval compute in 
+  let s1 := (Con_e (Ast.mkInd "" 0,1) enil) in
+  let s2 := (Con_e (Ast.mkInd "" 0,0) enil) in
+  let t := App_e (Var_e 0) (Var_e 1) in
+  (sbst s2 1 (sbst s1 0 t), sbst s1 0 (sbst s2 1 t)).
 
 Hint Constructors expression.eval.
 Hint Constructors expression.evals.

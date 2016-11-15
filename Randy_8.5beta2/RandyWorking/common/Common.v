286d4e1ed31373883c683aaa8f786575755ad5d5
rpollack <rpollack0@users.noreply.github.com>
2016-11-15 12:46:02 +0000

    Removed some old versions of mine.

--- Randy_8.5beta2/RandyWorking/common/Common.v
+++
@@ -1,7 +0,0 @@
-(** Collect all Common together **)
-
-Add LoadPath "." as Common.
-Require Export Template.Ast.
-Require Export Common.exceptionMonad.
-Require Export Common.RandyPrelude.
-Require Export Common.AstCommon.

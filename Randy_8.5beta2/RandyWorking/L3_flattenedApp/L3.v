d1de222a052558a97d1cd2dd1e6680ca790dd103
rpollack <rpollack0@users.noreply.github.com>
2016-11-15 12:46:02 +0000

    Removed some old versions of mine.

--- Randy_8.5beta2/RandyWorking/L3_flattenedApp/L3.v
+++
@@ -1,8 +0,0 @@
-(** Collect all of L3 together **)
-
-Add LoadPath "../common" as Common.
-Add LoadPath "../L1_MalechaQuoted" as L1.
-Add LoadPath "../L2_typeStrippedL1" as L2.
-Add LoadPath "." as L3.
-Require Export L3.term L3.program L3.wndEval L3.wcbvEval L3.wNorm 
-        L3.unaryApplications.

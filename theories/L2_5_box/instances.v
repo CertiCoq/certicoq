cf28c8735837c2b115f9c780a7725b944dfe51fa
Abhishek Anand <499314+aa755@users.noreply.github.com>
2016-11-17 19:00:41 +0000

    added instances: L2_5 language and L2 to L2_5 (total) translation.
    everything compiles now.

---
+++ theories/L2_5_box/instances.v
@@ -0,0 +1,27 @@
+Require Import L2_5.compile.
+(* Require Import L2.wcbvEval. *)
+Require Import certiClasses.
+Require Import Common.Common.
+
+(** FIX!! *)
+Instance bigStepOpSemL2_5_Term : BigStepOpSem (Program L2_5.compile.Term) :=
+  fun _ _ => True.
+
+(** FIX!! *)
+Instance WfL2_5_Term : WellFormed (Program L2_5.compile.Term) :=
+  fun _  => True.
+
+Instance certiL2_5 : CerticoqLanguage (Program L2_5.compile.Term) := {}.
+
+Local Generalizable Variable Lj.
+
+
+Instance certiL2_5_to_L3: 
+  CerticoqTotalTranslation (Program L2.compile.Term) (Program L2_5.compile.Term) :=
+L2_5.compile.L2Pgm_Program.
+
+
+
+
+
+

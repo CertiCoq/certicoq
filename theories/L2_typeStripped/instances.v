b367d325c19883ab71bb6713159c53550d50a913
rpollack <rpollack0@users.noreply.github.com>
2017-03-12 22:40:24 +0000

    Commiting what I've done in L2.instances for Abhishek to work on.

--- theories/L2_typeStripped/instances.v
+++ theories/L2_typeStripped/instances.v
@@ -5,7 +5,7 @@ Require Import certiClasses.
 Require Import Common.Common.
 Require Import L1g.instances.
 Require Import certiClasses2.
-Require Import stripEvalCommute.
+Require Import L2.stripEvalCommute.
 
 Instance bigStepOpSemL2Term: BigStepOpSem (Program L2.compile.Term) :=
   BigStepOpWEnv _ WcbvEval.
@@ -14,7 +14,6 @@ Instance bigStepOpSemL2Term: BigStepOpSem (Program L2.compile.Term) :=
  add them here. *)
 Instance WfL2Term: GoodTerm (Program L2.compile.Term) :=
   fun _  => True.
-
 
 Require Import SquiggleEq.UsefulTypes.
 Require Import DecidableClass.
@@ -62,6 +61,28 @@ Instance certiL1g_to_L2:
 
 Require Import certiClasses2.
 
+(***
+Goal
+  forall (p q:Program L1g.compile.Term),
+    bigStepOpSemL1gTerm p q ->
+    forall (q': Program Term),
+      bigStepOpSemL2Term (certiL1g_to_L2 p) q' ->
+              yesPreserved q q'.
+Proof.
+  intros. unfold yesPreserved. intros r. induction r.
+  - unfold questionHead. destruct q, q', p.
+    destruct QuestionHeadL1gTerm, QuestionHeadL2Term; try reflexivity.
+    unfold bigStepOpSemL1gTerm, BigStepOpWEnv in H.
+    unfold bigStepOpSemL2Term, BigStepOpWEnv in H0. cbn in *.
+    destruct H, H0. rewrite H1 in *. rewrite H2 in *. subst.
+    destruct main1, main0; cbn in *; try inversion H0.
+    + subst.
+    destruct main, main0; try reflexivity.  unfold BigStepOpWEnv in H. destruct H.
+    
+    destruct main, main0; cbn; try reflexivity; try destruct main1; try reflexivity.
+
+  
+***)
 
 Global Instance certiL1g_to_L2Correct :
   CerticoqTranslationCorrect certiL1g certiL2.
@@ -80,7 +101,7 @@ Proof.
     cbn. intros. destruct H0. destruct s, sv. cbn in H1. subst env.
     cbn in H0. cbn. clear H. (* ?? *)
     exists (stripProgram {| main := main0; env := env0 |}).
-    cbn. split. Set Printing All. split. 
+    cbn. split. split. 
     + apply (proj1 (stripEvalCommute.WcbvEval_hom _) _ _ H0).
     + reflexivity.
     + constructor.
@@ -94,7 +115,26 @@ Proof.
             destruct (PeanoNat.Nat.eqb n n0); try reflexivity.
           - cbn. destruct main0; cbn; try reflexivity.
             destruct main0_1; try reflexivity. }
-      * { intros. unfold observeNthSubterm,
+      * { intros. unfold observeNthSubterm.
+          destruct (ObsSubtermL1gTerm n (@mkPgm L1g.compile.Term main0 env0));
+            try constructor.
+          destruct (ObsSubtermL2Term
+                      n (stripProgram (@mkPgm L1g.compile.Term main0 env0))).
+          - constructor. constructor.
+            + unfold yesPreserved. intros. unfold questionHead.
+              destruct q.
+              * { destruct p, main1; try reflexivity.
+                  - admit.
+                  - admit. }
+              * case_eq (QuestionHeadL1gTerm Abs p);
+                case_eq (QuestionHeadL2Term Abs p0); intros; try reflexivity.
+                admit.
+            + intros. admit.
+          - admit.
+Admitted.
+
+
+(***************              
                   ObsSubtermL2Term, ObsSubtermL1gTerm.
           destruct main0; cbn; try constructor.
           - destruct main0_1; cbn; try constructor.
@@ -106,7 +146,7 @@ Proof.
             + admit.
           - admit. }
 Admitted.
-              
+******************)              
 
 
 (***************************

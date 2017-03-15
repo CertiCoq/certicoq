8b65faa25bdea7cfca487d27fbf99a14ff8a1ba2
Abhishek Anand <499314+aa755@users.noreply.github.com>
2017-03-15 20:50:35 +0000

    almost finished the proof of Randy's instance. The only problem
    is that the productivity check fails on a seemingly legit proof.

--- theories/L2_typeStripped/instances.v
+++ theories/L2_typeStripped/instances.v
@@ -61,29 +61,47 @@ Instance certiL1g_to_L2:
 
 Require Import certiClasses2.
 
-(***
-Goal
-  forall (p q:Program L1g.compile.Term),
-    bigStepOpSemL1gTerm p q ->
-    forall (q': Program Term),
-      bigStepOpSemL2Term (certiL1g_to_L2 p) q' ->
-              yesPreserved q q'.
+Lemma flattenAppCommutes : forall main,
+    (flattenApp (strip main)) =
+    (strip (fst (instances.flattenApp main)), List.map strip (snd (instances.flattenApp main))).
+Proof using.
+  destruct main; auto.
+  simpl. f_equal. f_equal.
+  induction t; auto.
+  simpl. f_equal. auto.
+Qed.
+
+Require Import Coq.btauto.Btauto.
+
+Lemma compileObsEq:
+  forall (main : L1g.compile.Term) (env : environ L1g.compile.Term),
+    {| main := main; env := env |} ⊑ stripProgram {| main := main; env := env |}.
 Proof.
-  intros. unfold yesPreserved. intros r. induction r.
-  - unfold questionHead. destruct q, q', p.
-    destruct QuestionHeadL1gTerm, QuestionHeadL2Term; try reflexivity.
-    unfold bigStepOpSemL1gTerm, BigStepOpWEnv in H.
-    unfold bigStepOpSemL2Term, BigStepOpWEnv in H0. cbn in *.
-    destruct H, H0. rewrite H1 in *. rewrite H2 in *. subst.
-    destruct main1, main0; cbn in *; try inversion H0.
-    + subst.
-    destruct main, main0; try reflexivity.  unfold BigStepOpWEnv in H. destruct H.
+  cofix.
+  intros. constructor.
+  - intros q. unfold questionHead, QuestionHeadL1gTerm, QuestionHeadL2Term.
+    simpl. rewrite flattenAppCommutes.
+    clear.
+    remember (fst (instances.flattenApp main)) as mm.
+    clear Heqmm. simpl.
+    clear main.
+    destruct mm, q; cbn; try reflexivity.
+    unfold implb.
+    btauto.
+  - intros ?.
+    unfold observeNthSubterm, ObsSubtermL1gTerm, ObsSubtermL2Term. simpl.
+    rewrite flattenAppCommutes.
+    destruct (instances.flattenApp main) as [f args].
+    simpl. clear main.
+    destruct f; cbn; try constructor.
+    Require Import SquiggleEq.list.
+    rewrite nth_error_map.
+    remember  (List.nth_error args n) as ln. setoid_rewrite <- Heqln.
+    clear Heqln. destruct ln; try constructor.
+    apply compileObsEq.
+    Fail idtac. (* We're done *)
+Admitted.
     
-    destruct main, main0; cbn; try reflexivity; try destruct main1; try reflexivity.
-
-  
-***)
-
 Global Instance certiL1g_to_L2Correct :
   CerticoqTranslationCorrect certiL1g certiL2.
 Proof.
@@ -101,122 +119,7 @@ Proof.
     cbn. intros. destruct H0. destruct s, sv. cbn in H1. subst env.
     cbn in H0. cbn. clear H. (* ?? *)
     exists (stripProgram {| main := main0; env := env0 |}).
-    cbn. split. split. 
+    cbn. split; [split; [ | reflexivity] | ]. 
     + apply (proj1 (stripEvalCommute.WcbvEval_hom _) _ _ H0).
-    + reflexivity.
-    + constructor.
-      * { unfold yesPreserved. intros. unfold stripProgram, implb.
-          unfold questionHead, QuestionHeadL2Term, QuestionHeadL1gTerm.
-          destruct q. cbn. destruct main0; cbn; try reflexivity.
-          - destruct main0_1; cbn; try reflexivity.
-            destruct (decide (i = i0)); cbn; try reflexivity.
-            destruct (PeanoNat.Nat.eqb n n0); try reflexivity.
-          - destruct (decide (i = i0)); cbn; try reflexivity.
-            destruct (PeanoNat.Nat.eqb n n0); try reflexivity.
-          - cbn. destruct main0; cbn; try reflexivity.
-            destruct main0_1; try reflexivity. }
-      * { intros. unfold observeNthSubterm.
-          destruct (ObsSubtermL1gTerm n (@mkPgm L1g.compile.Term main0 env0));
-            try constructor.
-          destruct (ObsSubtermL2Term
-                      n (stripProgram (@mkPgm L1g.compile.Term main0 env0))).
-          - constructor. constructor.
-            + unfold yesPreserved. intros. unfold questionHead.
-              destruct q.
-              * { destruct p, main1; try reflexivity.
-                  - admit.
-                  - admit. }
-              * case_eq (QuestionHeadL1gTerm Abs p);
-                case_eq (QuestionHeadL2Term Abs p0); intros; try reflexivity.
-                admit.
-            + intros. admit.
-          - admit.
-Admitted.
-
-
-(***************              
-                  ObsSubtermL2Term, ObsSubtermL1gTerm.
-          destruct main0; cbn; try constructor.
-          - destruct main0_1; cbn; try constructor.
-            induction n; induction (L1g.compile.Terms_list t); cbn.
-            + constructor. constructor. admit. admit.
-            + constructor. constructor. admit. admit.
-            + cbn. rewrite (proj2 (List.nth_error_None nil n)).
-              admit. admit.
-            + admit.
-          - admit. }
-Admitted.
-******************)              
-
-
-(***************************
-
-    + unfold yesPreserved. intros. unfold stripProgram, implb.
-      unfold questionHead, QuestionHeadL2Term, instances.QuestionHeadL2Term.
-      destruct q. cbn. destruct main0; cbn; try reflexivity.
-      * destruct main0_1; cbn; try reflexivity.
-        destruct (decide (i = i0)); cbn; try reflexivity.
-        destruct (PeanoNat.Nat.eqb n n0); try reflexivity.
-      * destruct (decide (i = i0)); cbn; try reflexivity.
-        destruct (PeanoNat.Nat.eqb n n0); try reflexivity.
-      * cbn. destruct main0; cbn; try reflexivity.
-        destruct main0_1; try reflexivity.
-    + intros. unfold observeNthSubterm. 
-      unfold ObsSubtermL2Term, instances.ObsSubtermL2Term.
-      destruct main0; cbn; try constructor.
-      * { destruct main0_1; cbn; try constructor.
-          induction n; cbn.
-          - constructor. constructor.
-            + unfold yesPreserved. intros. induction q.
-              destruct main0_2; cbn; try reflexivity.
-              * destruct main0_2_1; cbn; try reflexivity.
-                destruct (decide (i0 = i1)); cbn; try reflexivity.
-                destruct (PeanoNat.Nat.eqb n n2); reflexivity.
-              * destruct (decide (i0 = i1)); cbn; try reflexivity.
-                destruct (PeanoNat.Nat.eqb n n2); reflexivity.
-              * destruct main0_2; cbn; try reflexivity.
-                destruct main0_2_1; cbn; try reflexivity.
-            + intros. destruct main0_2; cbn; try constructor.
-              * { destruct main0_2_1; cbn; try constructor.
-                  destruct n; cbn.
-                  - constructor. constructor.
-                    + unfold yesPreserved. intros. destruct q.
-                
-                
-              * destruct (decide (i0 = ind2)); cbn; try reflexivity.
-              cbn.
-
-      
-      
-    exists (stripProgram sv). cbn. repeat split.
-    + apply (proj1 (stripEvalCommute.WcbvEval_hom (env s)) _ _ H0).
-    + destruct s, sv. cbn in H1. rewrite H1. cbn. reflexivity.
-    + admit.
-    + intros. unfold observeNthSubterm,
-              ObsSubtermL2Term, instances.ObsSubtermL2Term.
-      destruct sv. cbn. unfold flattenApp, instances.flattenApp.
-      destruct main; cbn; try constructor.
-      * { destruct (L1g.term.isConstruct_dec main1) as [[x0 [x1 [x2 j]]] | j].
-          - subst main1. cbn.
-            destruct (List.nth_error (main2 :: L1g.compile.Terms_list t) n).
-            + destruct
-                (List.nth_error
-                   (compile.strip main2 :: Terms_list (compile.strips t)) n).
-              * constructor. constructor. unfold yesPreserved. intros.
-                unfold implb. unfold questionHead.
-                destruct (instances.QuestionHeadL2Term
-                            q {| main := t0; env := env |}).
-                destruct q. cbn. destruct t1; cbn. cbn in H0. cbn in H1.
-                subst env.
-
-                
-                unfold instances.QuestionHeadL2Term.
-                destruct (instances.QuestionHeadL2Term
-                            q {| main := t0; env := env |}).
-                destruct q. cbn.
-
-                admit.
-              * cbn in H0. rewrite H1 in H0. cbn in H0.
-                rewrite j in H1. cbn in H1. subst env. subst main1.
-Admitted.    
-***************************)
+    + clear. apply compileObsEq.
+Qed.

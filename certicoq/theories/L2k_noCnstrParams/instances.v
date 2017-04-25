338c43c5d15701885cedf89a42cbfdfb5a3ab6a1
mattam@mattam.org <mattam@mattam.org@e2b0c78b-04ed-0310-b50c-832c51d4eb4a>
2017-04-25 21:59:17 +0000

    Minimal instances for L2k

--- certicoq/theories/L2k_noCnstrParams/instances.v
+++ certicoq/theories/L2k_noCnstrParams/instances.v
@@ -1,40 +1,39 @@
-Require Import L2.compile.
-Require Import L2.wcbvEval.
-Require Import L2.term.
+Require Import L2k.compile.
+Require Import L2k.wcbvEval.
+Require Import L2k.term.
 Require Import certiClasses.
 Require Import Common.Common.
-Require Import L1g.instances.
+Require Import L2.instances.
 Require Import certiClasses2.
-Require Import L2.stripEvalCommute.
+Require Import L2k.stripEvalCommute.
 
 
 (** If the compiler only correctly compiles terms with some properties,
  add them here. *)
-Instance WfL2Term: GoodTerm (Program L2.compile.Term) :=
+Instance WfL2Term: GoodTerm (Program L2k.compile.Term) :=
   fun _  => True.
 
 Require Import SquiggleEq.UsefulTypes.
 Require Import DecidableClass.
 
-Definition flattenApp (t:L2.compile.Term) :
-  (L2.compile.Term * (list L2.compile.Term)) :=
+Definition flattenApp (t:L2k.compile.Term) :
+  (L2k.compile.Term * (list L2k.compile.Term)) :=
   match t with
     | TApp fn arg args => (fn, cons arg (Terms_list args))
     | s => (s, nil)
   end.
 
-(***********
-Global Instance QuestionHeadL2Term: QuestionHead (Program L2.compile.Term) :=
+Global Instance QuestionHeadL2kTerm: QuestionHead (Program L2k.compile.Term) :=
   fun q t =>
     match q, fst (flattenApp (main t)) with
       | Cnstr ind ci, TConstruct ind2 ci2 _(*nargs*) =>
         andb (decide (ind=ind2)) (decide (ci=ci2))
       | Abs, TLambda _ _ => true
-      | _, _ => false 
+      | _, _ => false
     end.
 
-Global Instance ObsSubtermL2Term :
-  ObserveNthSubterm (Program L2.compile.Term) :=
+Global Instance ObsSubtermL2kTerm :
+  ObserveNthSubterm (Program L2k.compile.Term) :=
   fun n t =>
     match  (flattenApp (main t)) with
       | (TConstruct _ _ _ , subterms) =>
@@ -45,33 +44,33 @@ Global Instance ObsSubtermL2Term :
       | _ => None
     end.
 
-Global Instance certiL2Eval:
-  BigStepOpSem (Program L2.compile.Term) (Program L2.compile.Term).
+Global Instance certiL2kEval:
+  BigStepOpSem (Program L2k.compile.Term) (Program L2k.compile.Term).
 Proof.
   intros s sv. destruct s, sv. exact (WcbvEval env main main0 /\ env = env0).
 Defined.
 
-Global Instance certiL2: CerticoqLanguage (Program L2.compile.Term).
+Global Instance certiL2k: CerticoqLanguage (Program L2k.compile.Term).
 
-Instance certiL1g_to_L2: 
-  CerticoqTotalTranslation (Program L1g.compile.Term)
-                           (Program L2.compile.Term) :=
+Instance certiL2_to_L2k: 
+  CerticoqTotalTranslation (Program L2.compile.Term)
+                           (Program L2k.compile.Term) :=
   stripProgram.
 
 
 Require Import certiClasses2.
 
-Lemma flattenAppCommutes:
-  forall main : L1g.compile.Term,
-  flattenApp (L2.compile.strip main) =
-  (strip (fst (L1g.instances.flattenApp main)),
-   List.map strip (snd (L1g.instances.flattenApp main))).
-Proof using.
-  destruct main; auto.
-  simpl. f_equal. f_equal.
-  induction t; auto.
-  simpl. f_equal. auto.
-Qed.
+(* Lemma flattenAppCommutes: *)
+(*   forall main : L2.compile.Term, *)
+(*   flattenApp (L2k.compile.strip main) = *)
+(*   (strip (fst (L2.instances.flattenApp main)), *)
+(*    List.map strip (snd (L2.instances.flattenApp main))). *)
+(* Proof using. *)
+(*   destruct main; auto. *)
+(*   simpl. f_equal. f_equal. *)
+(*   induction t; auto. *)
+(*   simpl. f_equal. auto. *)
+(* Qed. *)
 
 Require Import Coq.btauto.Btauto.
 Require Import SquiggleEq.list.
@@ -102,7 +101,7 @@ Qed.
 Lemma identity_pres_yes:
   forall (p:Program Term), yesPreserved p p.
 Proof.
-  destruct p. unfold yesPreserved, questionHead, QuestionHeadL2Term; cbn.
+  destruct p. unfold yesPreserved, questionHead, QuestionHeadL2kTerm; cbn.
   destruct q.
   - destruct (fst (flattenApp main)); unfold implb; try reflexivity.
     btauto.
@@ -110,26 +109,23 @@ Proof.
 Qed.
 
 Lemma stripProgram_pres_yes:
-  forall (p:Program L1g.compile.Term), yesPreserved p (stripProgram p).
+  forall (p:Program L2.compile.Term), yesPreserved p (stripProgram p).
 Proof.
   destruct p. unfold stripProgram. cbn.
-  unfold yesPreserved, questionHead, QuestionHeadL1gTerm, QuestionHeadL2Term.
-  cbn. rewrite flattenAppCommutes.
-  remember (fst (L1g.instances.flattenApp main)) as mm. cbn.
-  destruct mm, q; cbn; try reflexivity.
-  unfold implb. btauto.
-Qed.
-
+  unfold yesPreserved, questionHead, QuestionHeadL2Term.
+  cbn.
+Admitted.
+(*
 Lemma my_stripProgram_pres_yes:
-  forall (main: L1g.compile.Term) (env: environ L1g.compile.Term) stv otv,
-    L1g.wcbvEval.WcbvEval env main stv ->
+  forall (main: L2.compile.Term) (env: environ L2.compile.Term) stv otv,
+    L2.wcbvEval.WcbvEval env main stv ->
     WcbvEval (stripEnv env) (strip main) otv ->
     yesPreserved {| main := stv; env := env |}
       {| main := otv; env := stripEnv env |}.
 Proof.
   intros.
-  pose proof (sac_sound H H0). subst.
-  unfold yesPreserved, questionHead, QuestionHeadL1gTerm, QuestionHeadL2Term.
+  (* pose proof (sac_sound H H0). subst. *)
+  unfold yesPreserved, questionHead, QuestionHeadL2Term, QuestionHeadL2kTerm.
   cbn. rewrite flattenAppCommutes.
   remember (fst (L1g.instances.flattenApp stv)) as mm. cbn.
   destruct mm, q; cbn; try reflexivity.

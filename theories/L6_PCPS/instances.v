57d00812fb16282bfc9f88e97b34bb1ab3dd5233
Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>
2019-03-06 16:28:13 -0500

    added dead param elim to the compiler pipeline and created tests that test the compilation up to that point with Katja

--- theories/L6_PCPS/instances.v
+++ theories/L6_PCPS/instances.v
@@ -8,7 +8,8 @@ From CertiCoq.Common Require Import certiClasses certiClassesLinkable Common.
 Require Import Coq.Unicode.Utf8.
 
 Require Import ZArith.
-From CertiCoq.L6 Require Import cps cps_util eval shrink_cps L5_to_L6 beta_contraction uncurry closure_conversion hoisting.
+From CertiCoq.L6 Require Import cps cps_util eval shrink_cps L5_to_L6 beta_contraction uncurry closure_conversion
+                                hoisting dead_param_elim.
 From CertiCoq.L7 Require Import L6_to_Clight.
 
 
@@ -127,7 +128,7 @@ Instance certiL5_t0_L6:
                                     next_iTag
                                     cenv nenv
         in
-        Ret ((M.empty _ , (add_cloTag bogus_cloTag bogus_cloiTag cenv'), nenv', M.empty _),  (M.empty _,   shrink_top t'))
+        Ret ((M.empty _ , (add_cloTag bogus_cloTag bogus_cloiTag cenv'), nenv', M.empty _),  (M.empty _,   dropper (shrink_top t')))
       | None => Exc "failed converting from L5 to L6"
       end)
     end.

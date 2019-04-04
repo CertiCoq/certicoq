d31a0a8a737e1faa48a40a049631524e75692e56
Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>
2019-04-03 21:37:26 -0400

    Tweaks in the dead param. elimination relation with Katja today

--- theories/L6_PCPS/dead_param_elim_rel.v
+++ theories/L6_PCPS/dead_param_elim_rel.v
@@ -30,73 +30,102 @@ match ys, bs with
   else drop_args ys' bs'
 | _, _ => ys
 end. 
-
+(* 
 Fixpoint Drop_args (ys : list var) (bs : list bool) (ys' : list var) : Prop := 
 let xs' := drop_args ys bs in
 if check_list_equality xs' ys' then True
 else False. 
+ *)
 
-Inductive Drop_body (f : var) (bs : list bool) : exp -> exp -> Prop := 
+
+(* S is the set of the dropped parameters of the function we're in. We need it
+   to make sure that these are not used anywhere in the program, and so the proof will go through.
+   Everything that we want to keep we want to make sure that is not in S.
+
+   the list of booleans is the drop parameters of the function we are calling and we need it so that
+   the application has the right arity.
+ *)
+Inductive Drop_args (S : Ensemble var) : list var -> list bool -> list var -> Prop :=
+| ADrop_nil : Drop_args S [] [] []
+| ADrop_cons_drop :
+    forall (x : var) (xs : list var) (bs: list bool) (ys : list var),
+      Drop_args S xs bs ys ->
+      Drop_args S (x :: xs) (false :: bs) ys
+| ADrop_cons_keep :
+    forall (x : var) (xs : list var) (bs: list bool) (ys : list var),
+      Drop_args S xs bs ys ->
+      ~ x \in S -> 
+      Drop_args S (x :: xs) (true :: bs) (x :: ys).
+
+
+(* S is the set of formal parameters that have been dropped from the *current* function.
+   That is, whatever is in S, is undefined in the environment when we execute the code. *)
+Inductive Drop_body (drop : var -> option (list bool)) (S : Ensemble var) :  exp -> exp -> Prop := 
 | BDrop_Constr : 
   forall (x : var) (ys : list var) (ct : cTag) (e : exp) (e' : exp), 
-  Drop_body f bs e e' ->
-  Drop_body f bs (Econstr x ct ys e') (Econstr x ct ys e')
-| BDrop_App_Same :
-  forall (ys : list var) (ys' : list var) (ft : fTag),
-  Drop_args ys bs ys' ->
-  Drop_body f bs (Eapp f ft ys) (Eapp f ft ys')
-| BDrop_App_Diff :
-  forall (ys : list var) (ft : fTag),
-  Drop_body f bs (Eapp f ft ys) (Eapp f ft ys)
-| BDrop_Halt : 
-  forall (x : var), 
-  Drop_body f bs (Ehalt x) (Ehalt x)
-(* Note that this case is impossible *)
-| BDrop_Fun : 
-  forall (B : fundefs) (e : exp), 
-  Drop_body f bs (Efun B e) (Efun B e)
+    Disjoint _ (FromList ys) S -> 
+    Drop_body drop S e e' ->
+    Drop_body drop S (Econstr x ct ys e') (Econstr x ct ys e')
 | BDrop_Prim : 
   forall (x : var) (g : prim) (ys : list var) (e : exp) (e' : exp), 
-  Drop_body f bs e e' ->
-  Drop_body f bs (Eprim x g ys e) (Eprim x g ys e')
+    Disjoint _ (FromList ys) S -> 
+    Drop_body drop S e e' ->
+    Drop_body drop S (Eprim x g ys e) (Eprim x g ys e')
 | BDrop_Proj : 
   forall (x : var) (ct : cTag) (n : N) (y : var) (e : exp) (e' : exp), 
-  Drop_body f bs e e' ->
-  Drop_body f bs (Eproj x ct n y e) (Eproj x ct n y e')
+    ~ y \in S ->
+    Drop_body drop S e e' ->
+    Drop_body drop S (Eproj x ct n y e) (Eproj x ct n y e')
 | BDrop_Case: 
   forall (x : var) (ce : list (cTag * exp)) (ce' : list (cTag * exp)),
-  (*Drop_case f bs ce ce' ->*)
-  Drop_body f bs (Ecase x ce) (Ecase x ce'). 
+    ~ x \in S ->
+    Forall2 (fun p1 p2 => Drop_body drop S (snd p1) (snd p2)) ce ce' -> 
+    Drop_body drop S (Ecase x ce) (Ecase x ce')
+| BDrop_Halt : 
+    forall (x : var), 
+      Drop_body drop S (Ehalt x) (Ehalt x)
+| BDrop_App_Unknown :
+    forall (f : var) (ys : list var) (ft : fTag),
+      (* Unknown function application, f is not in the list of top-level
+         function definitions. Therefore, it must be a parameter of the current
+         function *)
+      drop f = None -> 
+      Drop_body drop S (Eapp f ft ys) (Eapp f ft ys)
+| BDrop_App_Known :
+    forall (f : var) (ys : list var) (ys' : list var) (ft : fTag) (bs : list bool),
+      drop f = Some bs ->
+      Drop_args S ys bs ys' ->
+      Drop_body drop S (Eapp f ft ys) (Eapp f ft ys').
 
-Inductive Drop_case (f : var) (bs : list bool) : list (cTag * exp) -> list (cTag * exp) -> Prop := 
-| CDrop_nil : 
-  forall (x : var),
-  Drop_case f bs [] []
-| CDrop_cons : 
-  forall (ct : cTag) (e : exp) (e' : exp) (ce : list (cTag * exp)) (ce' : list (cTag * exp)), 
-  Drop_body f bs e e' ->
-  Drop_case f bs ce ce'->
-  Drop_case f bs ((ct, e) :: ce) ((ct, e') :: ce'). 
 
-Inductive Drop_body_fundefs (f : var) (bs : list bool) : fundefs -> fundefs -> Prop := 
+Inductive Drop_params : list var -> list bool -> list var -> Ensemble var -> Prop :=
+| PDrop_nil : Drop_params [] [] [] (Empty_set _)
+| PDrop_cons_drop :
+    forall (x : var) (xs : list var) (bs: list bool) (ys : list var) (S : Ensemble var),
+      Drop_params xs bs ys S ->
+      Drop_params (x :: xs) (false :: bs) ys (x |: S)
+| PDrop_cons_keep :
+    forall (x : var) (xs : list var) (bs: list bool) (ys : list var) (S : Ensemble var),
+      Drop_params xs bs ys S ->
+      Drop_params (x :: xs) (true :: bs) (x :: ys) S.
+
+
+Inductive Drop_body_fundefs (drop : var -> option (list bool)) : fundefs -> fundefs -> Prop := 
 | FDrop_nil : 
-  Drop_body_fundefs f bs Fnil Fnil
+  Drop_body_fundefs drop Fnil Fnil
 | FDrop_cons : 
-  forall (F : fundefs) (F' : fundefs) (e : exp) (e' : exp) (xs : list var)
-  (g : var) (ft : fTag), 
-  Drop_body f bs e e' ->
-  Drop_body_fundefs f bs F F' ->
-  Drop_body_fundefs f bs (Fcons g ft xs e F) (Fcons g ft xs e' F'). 
+  forall (F : fundefs) (F' : fundefs) (e : exp) (e' : exp) (xs ys : list var) (bs : list bool)
+    (g : var) (ft : fTag) (S : Ensemble var), 
+    drop g = Some bs ->  
+    Drop_params xs bs ys S -> 
+    Drop_body drop S e e' ->
+    Drop_body_fundefs drop F F' ->
+    Drop_body_fundefs drop (Fcons g ft xs e F) (Fcons g ft ys e' F'). 
 
-Inductive Drop (S : Ensemble var) : fundefs -> exp -> fundefs -> exp -> Prop := 
-| Drop_Fnil : forall (e : exp) , Drop S Fnil e Fnil e
-| Drop_Fcons : 
-  forall (F : fundefs) (F' : fundefs) (F'' : fundefs) (e : exp) (e' : exp) (e'' : exp)
-  (f : var) (xs : list var) (xs' : list var) (bs : list bool) 
-  (ef : exp) (ef' : exp) (ft : fTag), 
-  Drop S F e F' e' -> 
-  Filter S xs xs' ->
-  Drop_body f bs ef ef' ->
-  Drop_body f bs e' e'' ->
-  Drop_body_fundefs f bs F' F'' ->
-  Drop S (Fcons f ft xs ef F) e (Fcons f ft xs' ef' F'') e''. 
+
+Inductive Drop (drop : var -> option (list bool)) : fundefs -> exp -> fundefs -> exp -> Prop := 
+| Drop_toplevel :
+    forall B e B' e',
+      Drop_body_fundefs drop B B' ->
+      Drop_body drop (Empty_set _) e e' -> 
+      Drop drop B e B' e. 

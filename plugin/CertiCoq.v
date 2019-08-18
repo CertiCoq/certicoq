4fcd71f3eb84e83a4c951a1c4fb9b5fb4a534a5c
Matthieu Sozeau <mattam@mattam.org>
2019-08-18 12:20:45 +0200

    Fix plugin compilation

--- plugin/CertiCoq.v
+++ plugin/CertiCoq.v
@@ -1,10 +1,11 @@
-Require Import String Template.Ast Template.TemplateMonad Template.TemplateMonad.Extractable.
+Require Import String.
+From MetaCoq.Template Require Import All.
 
-
-Declare ML Module "template_coq".
-Declare ML Module "template_coq_checker_plugin".
-Declare ML Module "template_coq_pcuic_plugin".
-Declare ML Module "template_coq_extraction_plugin".
+Declare ML Module "metacoq_template_plugin".
+Declare ML Module "metacoq_checker_plugin".
+Declare ML Module "metacoq_pcuic_plugin".
+Declare ML Module "metacoq_safechecker_plugin".
+Declare ML Module "metacoq_erasure_plugin".
 Declare ML Module "certicoq_plugin".
 
 Global Unset Template Cast Propositions.

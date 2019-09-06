3972004a667e1349f360b6849e211f7a6b187934
Matthieu Sozeau <mattam@mattam.org>
2019-09-06 15:59:17 +0200

    Update plugin test file (not working with universes yet)

--- benchmarks/test_plugin.v
+++ benchmarks/test_plugin.v
@@ -1,14 +1,24 @@
 Require Import Arith.
 From CertiCoq.Plugin Require Import CertiCoq.
 Unset Template Cast Propositions.
-
+Require Import List.
+Import ListNotations.
 Definition foo := 3 + 4.
 
+MetaCoq Check foo.
+MetaCoq Erase foo.
 CertiCoq Compile foo.
 
+Definition bool_list := map negb [true; false].
+Set Printing Universes.
+(* Universe issues: undeclared universes from sections *)
+(* MetaCoq SafeCheck bool_list.*)
+
 Require Import Binom.
+(* Universe issues: template polymorphism not implemented yet *)
 
-CertiCoq Compile main.
+(* MetaCoq Erase main. *)
+(* CertiCoq Compile main. *)
 
-Require Import CertiCoq.Benchmarks.vs.
-CertiCoq Compile main.
+(* Require Import CertiCoq.Benchmarks.vs. *)
+(* CertiCoq Compile main. *)

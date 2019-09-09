7805a4dd7182a81cb8a28ab5f4e312129d2adefe
Matthieu Sozeau <mattam@mattam.org>
2019-09-09 17:28:58 +0200

    Update CertiCoq to latest Template-Coq

--- plugin/CertiCoq.v
+++ plugin/CertiCoq.v
@@ -3,7 +3,7 @@ From MetaCoq.Template Require Import All.
 
 Declare ML Module "metacoq_template_plugin".
 Declare ML Module "metacoq_checker_plugin".
-Declare ML Module "metacoq_pcuic_plugin".
+(* Declare ML Module "metacoq_pcuic_plugin". *)
 Declare ML Module "metacoq_safechecker_plugin".
 Declare ML Module "metacoq_erasure_plugin".
 Declare ML Module "certicoq_plugin".

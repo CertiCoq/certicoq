f468ed5af6a61141aff86ab6840536869310634b
Joomy Korkut <joomy@cattheory.com>
2019-10-04 07:53:31 -0400

    Change names in cps.v

--- theories/L6_PCPS/cps.v
+++ theories/L6_PCPS/cps.v
@@ -15,18 +15,18 @@ Import ListNotations.
 
 Module M := Maps.PTree.
 
-Fixpoint getlist {A} (xs: list M.elt) (rho: M.t A) : option (list A) :=
+Fixpoint get_list {A} (xs: list M.elt) (rho: M.t A) : option (list A) :=
   match xs with
-  | x :: xs' => match M.get x rho, getlist xs' rho with
+  | x :: xs' => match M.get x rho, get_list xs' rho with
                | Some v, Some vs => Some (v::vs)
                | _, _ => None
                end
   | nil => Some nil
   end.
 
-Fixpoint setlist {A} (xs: list M.elt) (vs: list A) (rho: M.t A) : option (M.t A) :=
+Fixpoint set_list {A} (xs: list M.elt) (vs: list A) (rho: M.t A) : option (M.t A) :=
   match xs, vs with
-  | x::xs', v::vs' => match setlist xs' vs' rho with
+  | x::xs', v::vs' => match set_list xs' vs' rho with
                      | Some rho' => Some (M.set x v rho')
                      | None => None
                      end
@@ -34,11 +34,11 @@ Fixpoint setlist {A} (xs: list M.elt) (vs: list A) (rho: M.t A) : option (M.t A)
   | _, _ => None
   end.
 
-Definition var  := M.elt. (* value variables *)
-Definition fTag := M.elt. (* discrimination tags for functions *)
-Definition iTag := M.elt. (* discrimination tags for inductive types *)
-Definition cTag := M.elt. (* discrimination tags for constructors *)
-Definition prim := M.elt. (* primitive operators *)
+Definition var      := M.elt. (* value variables *)
+Definition fun_tag  := M.elt. (* discrimination tags for functions *)
+Definition ind_tag  := M.elt. (* discrimination tags for inductive types *)
+Definition ctor_tag := M.elt. (* discrimination tags for constructors *)
+Definition prim     := M.elt. (* primitive operators *)
 
 
 (* Remark.  It sure would be nice if we could use abstraction here,
@@ -59,7 +59,7 @@ Definition prim := M.elt. (* primitive operators *)
 
 
 (* Given a list of tagged variants, return the one with the matching tag. *)
-Fixpoint findtag {A} (cl: list (cTag * A)) (c: cTag) : option A :=
+Fixpoint findtag {A} (cl: list (ctor_tag * A)) (c: ctor_tag) : option A :=
   match cl with
   | (c',a)::cl' => if M.elt_eq c' c then Some a else findtag cl' c
   | nil => None
@@ -69,17 +69,16 @@ Fixpoint findtag {A} (cl: list (cTag * A)) (c: cTag) : option A :=
 
 (* Expressions [exp] of the CPS language. *)
 Inductive exp : Type :=
-| Econstr: var -> cTag -> list var -> exp -> exp
-| Ecase: var -> list (cTag * exp) -> exp
-| Eproj: var -> cTag -> N -> var -> exp -> exp
+| Econstr: var -> ctor_tag -> list var -> exp -> exp
+| Ecase: var -> list (ctor_tag * exp) -> exp
+| Eproj: var -> ctor_tag -> N -> var -> exp -> exp
 | Efun: fundefs -> exp -> exp
-| Eapp: var -> fTag -> list var -> exp
+| Eapp: var -> fun_tag -> list var -> exp
 | Eprim: var -> prim -> list var -> exp -> exp (* where prim is id *)
 | Ehalt : var -> exp
 with fundefs : Type :=
-| Fcons: var -> fTag -> list var -> exp -> fundefs -> fundefs
+| Fcons: var -> fun_tag -> list var -> exp -> fundefs -> fundefs
 | Fnil: fundefs.
-
 
 (* [Econstr x t c ys e] applies a data constructor with tag [c] to
            a list of values denoted by variables [ys].   The resulting
@@ -125,15 +124,15 @@ with fundefs : Type :=
 
 Lemma exp_ind' :
   forall P : exp -> Type,
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
     (forall (v : var), P (Ecase v nil)) ->
-    (forall (v : var) (l : list (cTag * exp)) (c : cTag) (e : exp),
+    (forall (v : var) (l : list (ctor_tag * exp)) (c : ctor_tag) (e : exp),
         P e -> P (Ecase v l) -> P (Ecase v ((c, e) :: l))) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall (f2 : fundefs) (e : exp), P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var)  (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
@@ -153,36 +152,36 @@ Qed.
 (** Mutual induction scheme for exp and fundefs *)
 Lemma exp_mut :
   forall (P : exp -> Type) (P0 : fundefs -> Type),
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
     (forall (v : var), P (Ecase v nil)) ->
-    (forall (v : var) (l : list (cTag * exp)) (c : cTag) (e : exp),
+    (forall (v : var) (l : list (ctor_tag * exp)) (c : ctor_tag) (e : exp),
         P e -> P (Ecase v l) -> P (Ecase v ((c, e) :: l))) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall f2 : fundefs, P0 f2 -> forall e : exp, P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var) (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
-    (forall (v : var) (t : fTag) (l : list var) (e : exp),
+    (forall (v : var) (t : fun_tag) (l : list var) (e : exp),
         P e -> forall f5 : fundefs, P0 f5 -> P0 (Fcons v t l e f5)) ->
     P0 Fnil -> forall e : exp, P e
 with fundefs_mut :
   forall (P : exp -> Type) (P0 : fundefs -> Type),
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
     (forall (v : var), P (Ecase v nil)) ->
-    (forall (v : var) (l : list (cTag * exp)) (c : cTag) (e : exp),
+    (forall (v : var) (l : list (ctor_tag * exp)) (c : ctor_tag) (e : exp),
         P e -> P (Ecase v l) -> P (Ecase v ((c, e) :: l))) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall f2 : fundefs, P0 f2 -> forall e : exp, P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var) (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
-    (forall (v : var) (t : fTag) (l : list var) (e : exp),
+    (forall (v : var) (t : fun_tag) (l : list var) (e : exp),
         P e -> forall f5 : fundefs, P0 f5 -> P0 (Fcons v t l e f5)) ->
     P0 Fnil -> forall f7 : fundefs, P0 f7.
 Proof.
@@ -205,34 +204,34 @@ Qed.
 
 Lemma exp_mut_alt :
   forall (P : exp -> Prop) (P0 : fundefs -> Prop),
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
-    (forall (v : var) (l : list (cTag * exp)),
+    (forall (v : var) (l : list (ctor_tag * exp)),
        Forall (fun x => P (snd x)) l -> P (Ecase v l)) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall f2 : fundefs, P0 f2 -> forall e : exp, P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var) (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
-    (forall (v : var) (t : fTag) (l : list var) (e : exp),
+    (forall (v : var) (t : fun_tag) (l : list var) (e : exp),
         P e -> forall f5 : fundefs, P0 f5 -> P0 (Fcons v t l e f5)) ->
     P0 Fnil -> forall e : exp, P e
 with fundefs_mut_alt :
   forall (P : exp -> Prop) (P0 : fundefs -> Prop),
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
-    (forall (v : var) (l : list (cTag * exp)),
+    (forall (v : var) (l : list (ctor_tag * exp)),
         Forall (fun x => P (snd x)) l -> P (Ecase v l)) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall f2 : fundefs, P0 f2 -> forall e : exp, P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var) (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
-    (forall (v : var) (t : fTag) (l : list var) (e : exp),
+    (forall (v : var) (t : fun_tag) (l : list var) (e : exp),
         P e -> forall f5 : fundefs, P0 f5 -> P0 (Fcons v t l e f5)) ->
     P0 Fnil -> forall f7 : fundefs, P0 f7.
 Proof.
@@ -255,19 +254,19 @@ Qed.
 (* to do proofs simultaneously. *)
 Lemma exp_def_mutual_ind :
   forall (P : exp -> Prop) (P0 : fundefs -> Prop),
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
     (forall (v : var), P (Ecase v nil)) ->
-    (forall (v : var) (l : list (cTag * exp)) (c : cTag) (e : exp),
+    (forall (v : var) (l : list (ctor_tag * exp)) (c : ctor_tag) (e : exp),
         P e -> P (Ecase v l) -> P (Ecase v ((c, e) :: l))) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall f2 : fundefs, P0 f2 -> forall e : exp, P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var) (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
-    (forall (v : var) (t : fTag) (l : list var) (e : exp),
+    (forall (v : var) (t : fun_tag) (l : list var) (e : exp),
         P e -> forall f5 : fundefs, P0 f5 -> P0 (Fcons v t l e f5)) ->
     P0 Fnil -> (forall e : exp, P e) /\ (forall f : fundefs, P0 f).
 Proof.
@@ -278,18 +277,18 @@ Qed.
 
 Lemma exp_def_mutual_ind' :
   forall (P : exp -> Prop) (P0 : fundefs -> Prop),
-    (forall (v : var) (t : cTag) (l : list var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (l : list var) (e : exp),
         P e -> P (Econstr v t l e)) ->
-    (forall (v : var) (l : list (cTag * exp)),
+    (forall (v : var) (l : list (ctor_tag * exp)),
         Forall (fun x => P (snd x)) l -> P (Ecase v l)) ->
-    (forall (v : var) (t : cTag) (n : N) (v0 : var) (e : exp),
+    (forall (v : var) (t : ctor_tag) (n : N) (v0 : var) (e : exp),
         P e -> P (Eproj v t n v0 e)) ->
     (forall f2 : fundefs, P0 f2 -> forall e : exp, P e -> P (Efun f2 e)) ->
-    (forall (v : var) (t : fTag) (l : list var), P (Eapp v t l)) ->
+    (forall (v : var) (t : fun_tag) (l : list var), P (Eapp v t l)) ->
     (forall (v : var) (p : prim) (l : list var) (e : exp),
         P e -> P (Eprim v p l e)) ->
     (forall (v : var), P (Ehalt v)) ->
-    (forall (v : var) (t : fTag) (l : list var) (e : exp),
+    (forall (v : var) (t : fun_tag) (l : list var) (e : exp),
         P e -> forall f5 : fundefs, P0 f5 -> P0 (Fcons v t l e f5)) ->
     P0 Fnil -> (forall e : exp, P e) /\ (forall f : fundefs, P0 f).
 Proof.
@@ -316,7 +315,7 @@ Ltac exp_defs_induction IH1 IHl IH2 :=
 (** * CPS Values *)
 
 Inductive val : Type :=
-| Vconstr : cTag -> list val -> val
+| Vconstr : ctor_tag -> list val -> val
 | Vfun : M.t val -> fundefs -> var -> val
 (* [Vfun env fds f]
      where env is the environment at the function binding site
@@ -327,8 +326,8 @@ Inductive val : Type :=
 (** Induction principle for values. *)
 Lemma val_ind' :
   forall P : val -> Prop,
-    (forall (t : cTag), P (Vconstr t nil)) ->
-    (forall (t : cTag) (v : val) (l : list val),
+    (forall (t : ctor_tag), P (Vconstr t nil)) ->
+    (forall (t : ctor_tag) (v : val) (l : list val),
         P v -> P (Vconstr t l) -> P (Vconstr t (v :: l))) ->
     (forall (t : M.t val) (f0 : fundefs) (v : var), P (Vfun t f0 v)) ->
     (forall z : Z, P (Vint z)) ->
@@ -354,32 +353,49 @@ Fixpoint find_def (f: var) (fl:  fundefs) :=
   | Fnil => None
   end.
 
-(** * Information associated with identifiers Identifier info **)
+(** * Information associated with identifiers **)
 
-(** The info of a constructor. Includes: the name of the constructor
-                                         the name of its inductive type
-                                         iTag of corresponding inductive type
-                                         the constructor's arity
-                                         the cTags ordinal in inductive defn starting at zero *)
-
-Definition cTyInfo : Type := name * name * iTag * N * N.
+(* The info of a constructor. *)
+Record ctor_ty_info : Type :=
+  Build_ctor_ty_info
+    { ctor_name     : name    (* the name of the constructor *)
+    ; ctor_ind_name : name    (* the name of its inductive type *)
+    ; ctor_ind_tag  : ind_tag (* ind_tag of corresponding inductive type *)
+    ; ctor_arity    : N       (* the arity of the constructor *)
+    ; ctor_ordinal  : N       (* the [ctor_tag]s ordinal in inductive defn starting at zero *)
+    }.
 
 (* The info of an inductive type is list of the ctags of its constructors *)
-Definition iTyInfo : Type := list (cTag * N).
+Definition ind_ty_info : Type := list (ctor_tag * N).
 
-Definition unknown_cTyInfo : cTyInfo := (nAnon, nAnon, 1%positive, 0%N, 0%N).
+Definition unknown_ctor_ty_info : ctor_ty_info :=
+  {| ctor_name     := nAnon
+   ; ctor_ind_name := nAnon
+   ; ctor_ind_tag  := 1%positive
+   ; ctor_arity    := 0%N
+   ; ctor_ordinal  := 0%N
+  |}.
 
-Definition unknown_iTyInfo : iTyInfo := nil.
+Definition unknown_ind_ty_info : ind_ty_info := nil.
 
-Definition cEnv := M.t cTyInfo.  (* An constructor environment maps [cTag]s to their information *)
+(* An constructor environment maps [ctor_tag]s to their information *)
+Definition ctorEnv := M.t ctor_ty_info.
 
-Definition iEnv := M.t iTyInfo. (* An inductive type environment maps [iTag]s to their constructors with their arities *)
+(* An inductive type environment maps [ind_tag]s to their constructors with their arities *)
+Definition ind_env := M.t ind_ty_info.
 
-Definition fTyInfo : Type := N * list N. (* every calling convention requires knowing how many
-                                            arguments in which slots of the arg array *)
+(* Every calling convention requires knowing
+   how many arguments in which slots of the arg array *)
+Definition fun_ty_info : Type := N * list N.
 
-Definition fEnv: Type := M.t fTyInfo.
+Definition fun_env : Type := M.t fun_ty_info.
 
 (** Register the tag used for closures **)
-Definition add_cloTag (c i : positive) (cenv : cEnv) : cEnv :=
-  M.set c (nAnon, nAnon, i, 2%N, 0%N) cenv.
+Definition add_closureTag (c i : positive) (cenv : ctorEnv) : ctorEnv :=
+  let info := {| ctor_name     := nAnon
+               ; ctor_ind_name := nAnon
+               ; ctor_ind_tag  := i
+               ; ctor_arity    := 2%N
+               ; ctor_ordinal  := 0%N
+              |}
+  in M.set c info cenv.

fccb8e5f0b630058b8114182846e82f8a562d719
Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>
2019-12-31 12:41:18 -0500

    Some diagntics about CPS pipeline being too slow

--- benchmarks/tests.v
+++ benchmarks/tests.v
@@ -39,8 +39,34 @@ Definition vs_hard :=
   | _ => false
   end.
 
-CertiCoq Compile vs_easy. (* XXX this takes too long and needs investigation *)
-CertiCoq Compile "anf"  vs_easy.
+CertiCoq Compile "time" vs_easy. 
+CertiCoq Compile "time" "anf"  vs_easy.
+
+(* Zoe: Compiling with the CPS pipeline takes much longer for vs_easy.
+   The overhead seems to come from the C translation: (maybe has to do with dbg/error messages?)
+
+Timing for CPS:
+Debug: Time elapsed in L1g:  8.835582
+Debug: Time elapsed in L2k:  0.000454
+Debug: Time elapsed in L2k_eta:  0.000620
+Debug: Time elapsed in L4:  0.014821
+Debug: Time elapsed in L4_2:  0.003420
+Debug: Time elapsed in L4_5:  0.000780
+Debug: Time elapsed in L5:  0.005000
+Debug: Time elapsed in L6 CPS:  0.105993
+Debug: Time elapsed in L6 Pipeline:  8.532707
+Debug: Time elapsed in L7:  87.985509
+
+Timing for ANF:
+Debug: Time elapsed in L1g:  8.543669
+Debug: Time elapsed in L2k:  0.000457
+Debug: Time elapsed in L2k_eta:  0.000640
+Debug: Time elapsed in L4:  0.013329
+Debug: Time elapsed in L6 ANF:  0.020384
+Debug: Time elapsed in L6 Pipeline:  0.148308
+Debug: Time elapsed in L7:  2.394216 *)
+
+
 
 CertiCoq Compile vs_hard.
 CertiCoq Compile "anf" vs_hard.

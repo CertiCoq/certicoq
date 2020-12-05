21d5cfbacb418ec088f0fd2b4cf759682b9ba352
Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>
2020-12-05 18:43:28 -0500

    Example with int63

--- benchmarks/axioms/tests.v
+++ benchmarks/axioms/tests.v
@@ -7,6 +7,7 @@ Require Import ExtLib.Data.String.
 
 Import ListNotations. 
 
+(* Example 1: printing axioms *)
 Axiom (print_nat : nat -> unit).
 Axiom (print_str : string -> unit).
 Axiom (new_line : unit -> unit).
@@ -32,3 +33,36 @@ Definition print_lst := print_list [1;2;3;4;5].
 CertiCoq Compile -direct print_lst
 Extract Constants [ print_nat => "print_gallina_nat", print_str => "print_gallina_string", new_line => "print_new_line" ]
 Include [ "print.h" ].
+
+
+(* Example 2: int 63 *)
+
+Axiom (int63 : Type).
+Axiom (add : int63 -> int63 -> int63).
+Axiom (minus : int63 -> int63 -> int63).
+Axiom (mul : int63 -> int63 -> int63).
+Axiom (print_int63 : int63 -> unit).
+
+Axiom (zero_int63 : int63).
+Axiom (one_int63 : int63).
+
+
+Fixpoint fib_aux (n : nat) (prev0 prev1 : int63) :=
+  match n with
+  | 0 => add prev0 prev1
+  | S n => fib_aux n (add prev0 prev1) prev0
+  end.
+
+Fixpoint fib (n : nat) :=
+  match n with
+  | 0 => zero_int63
+  | _ => fib_aux (n - 1) zero_int63 one_int63
+  end.
+
+Definition fibn : unit :=
+  let _ := print_int63 (fib 11) in
+  new_line tt.
+
+CertiCoq Compile -direct fibn
+Extract Constants [ add => "add_int63", zero_int63 => "zero_int63", one_int63 => "one_int63", print_int63 => "print_int63", new_line => "print_new_line" ]
+Include [ "int63.h" ].

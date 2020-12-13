ee36b10c1538dfd6a222de967c04fe3914275ad0
Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>
2020-12-12 19:26:22 -0500

    More cleaning

--- theories/common/ExtLibMisc.v
+++
@@ -1,9 +0,0 @@
-Require Import ExtLib.Structures.Monads.
-
-Import Monad.MonadNotation.
-Open Scope monad_scope.
-Require Import Coq.Lists.List.
-Import ListNotations.
-
-
-(* the content from here moved to SquiggleEq.ExtLibMisc. *)

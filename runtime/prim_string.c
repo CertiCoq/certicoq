ef76279e1c44e6d1e31b69bbf28238bfa682ad20
Yannick Forster <yannick.forster@inria.fr>
2025-01-29 15:14:31 +0100

    fix bootstrap

--- runtime/prim_string.c
+++ runtime/prim_string.c
@@ -5,28 +5,23 @@
 
 typedef value primstring;
 
+<<<<<<< HEAD
 #define trace(...) // printf(__VA_ARGS__)
+=======
+#define trace(...) printf(__VA_ARGS__)
+>>>>>>> bacd3660 (fix bootstrap)
 
 value prim_string_compare(primstring x, primstring y)
 {
   /* register signed long long xr = Unsigned_long_val(x); */
   /* register signed long long yr = Unsigned_long_val(y); */
   /* register signed long long result = xr - yr; */
-<<<<<<< HEAD
   trace("Calling prim_string_compare\n");
   /* trace("Calling prim_string_compare on %llu and %llu: %lli \n", x, y); */
   /* if (result == 0) return 1; */
   /* else if (result < 0) return 3; */
   /* else */
     return 1;
-=======
-  /* trace("Calling prim_int63_compare\n"); */
-  /* trace("Calling prim_int63_compare on %llu and %llu: %lli \n", xr, yr, result); */
-  /* if (result == 0) return 1; */
-  /* else if (result < 0) return 3; */
-  /* else */
-    return 5;
->>>>>>> b63c45cd (strings)
 }
 
 primint prim_string_get(primstring x, primint y)
@@ -34,20 +29,12 @@ primint prim_string_get(primstring x, primint y)
   /* register signed long long xr = Unsigned_long_val(x); */
   /* register signed long long yr = Unsigned_long_val(y); */
   /* register signed long long result = xr - yr; */
-<<<<<<< HEAD
   trace("Calling prim_string_get\n");
-=======
-  /* trace("Calling prim_int63_compare\n"); */
->>>>>>> b63c45cd (strings)
   /* trace("Calling prim_int63_compare on %llu and %llu: %lli \n", xr, yr, result); */
   /* if (result == 0) return 1; */
   /* else if (result < 0) return 3; */
   /* else */
-<<<<<<< HEAD
     return 97;
-=======
-    return 5;
->>>>>>> b63c45cd (strings)
 }
 
 primint prim_string_length(primstring x)
@@ -55,18 +42,11 @@ primint prim_string_length(primstring x)
   /* register signed long long xr = Unsigned_long_val(x); */
   /* register signed long long yr = Unsigned_long_val(y); */
   /* register signed long long result = xr - yr; */
-<<<<<<< HEAD
   trace("Calling prim_string_length\n");
-=======
-  /* trace("Calling prim_int63_compare\n"); */
->>>>>>> b63c45cd (strings)
   /* trace("Calling prim_int63_compare on %llu and %llu: %lli \n", xr, yr, result); */
   /* if (result == 0) return 1; */
   /* else if (result < 0) return 3; */
   /* else */
-<<<<<<< HEAD
   return 0;
-=======
-  return 5;
->>>>>>> b63c45cd (strings)
+>>>>>>> bacd3660 (fix bootstrap)
 }

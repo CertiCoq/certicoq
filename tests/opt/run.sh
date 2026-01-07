d8c117a1a815b0f8792bd413596910b1549c9316
Mihalis Rasoulis <rassmike@mgmail.com>
2026-01-07 21:22:26 +0200

    Remove benchmarking capabilities from optimizations' tests

--- tests/opt/run.sh
+++ tests/opt/run.sh
@@ -8,22 +8,22 @@ for f in $FILES
 do
     if [ -f "${f}_opt" ]; then
         echo "Running ${f} in direct-style with O1"
-        ./${f}_opt $1
+        ./${f}_opt 1
     fi
     if [ -f "${f}_cps_opt" ]; then
         echo "Running ${f} in CPS with O1"
-        ./${f}_cps_opt $1
+        ./${f}_cps_opt 1
     fi
 
     for i in {1..5}
     do
 	if [ -f "${f}_opt${i}" ]; then
             echo "Running ${f} in direct-style with O1"
-            ./${f}_opt${i} $1
+            ./${f}_opt${i} 1
 	fi
 	if [ -f "${f}_cps_opt${i}" ]; then
             echo "Running ${f} in CPS with O1"
-            ./${f}_cps_opt${i} $1
+            ./${f}_cps_opt${i} 1
 	fi
     done
 done

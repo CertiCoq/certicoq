52e025a8a524c6c9f5a60bd6a93d1235d2d8d24e
Mihalis Rasoulis <rassmike@mgmail.com>
2026-02-04 18:35:33 +0200

    Compare axioms and programs results against exptected outputs

--- tests/axioms/run.sh
+++ tests/axioms/run.sh
@@ -15,7 +15,8 @@ for f in $FILES
 do
     if [ -x ${f} ]; then
 	echo "Running ${f} ${N} time(s)."
-	./${f} ${N} || exit 1
+	./${f} ${N} > /tmp/${f}.txt
+	diff /tmp/${f}.txt ./expected_outputs/${f}.txt || exit 1
     else
 	echo "${f} not found."
     fi
--- tests/programs/run.sh
+++ tests/programs/run.sh
@@ -2,13 +2,14 @@
 
 FILES=`cat TESTS`
 
-echo "Running each test ${1} times"
+echo "Running tests"
 
 for f in $FILES
 do
     if [ -f "${f}_opt" ]; then
         echo "Running ${f} in direct-style with O1"
         ./${f}_opt $1 > /tmp/${f}_opt.txt
+	diff /tmp/${f}_opt.txt ./expected_outputs/${f}.txt || exit 1
     fi
 
     for i in {1..5}
@@ -16,7 +17,7 @@ do
 	if [ -f "${f}_opt${i}" ]; then
             echo "Running ${f} in direct-style with O1"
             ./${f}_opt${i} $1 > /tmp/${f}_opt${i}.txt
-	    diff /tmp/${f}_opt.txt /tmp/${f}_opt${i}.txt || exit 1
+	    diff /tmp/${f}_opt${i}.txt ./expected_outputs/${f}.txt || exit 1
 	fi
     done
 done
